046fd803c037f801f68f3f789f49a7cd95a558d3
Mark Alperin <mark.t.alperin@gmail.com>
2022-01-08 14:17:16 -0600

    created update feed button

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 $(document).ready(function(){
   var $app = $('#app');
   $app.html('');
+
+
   var $title = $('<h1>Twiddler</h1>');
   $title.appendTo($app);
   $title.on("click", function(event) {
@@ -10,14 +12,23 @@ $(document).ready(function(){
     alert('This is an event: ' + event.target.innerText);
   });
 
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div class="tweet"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($app);
-    index -= 1;
-  }
+  var $updateFeedButton = $('<button id="update-feed" class="button">Update Feed</button>');
+  $updateFeedButton.appendTo($app);
+  $updateFeedButton.on("click", function () {
+    $updateFeed();
+  })
+
+  var $updateFeed = function() {
+    var index = streams.home.length - 1;
+    while(index >= 0){
+      var tweet = streams.home[index];
+      var $tweet = $('<div class="tweet"></div>');
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      $tweet.appendTo($app);
+      index -= 1;
+    }
+  };
+  $updateFeed();
 
 
 
