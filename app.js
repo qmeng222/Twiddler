3a8aa7a68ff6b7420d5b53c22bb48e8d32f072d4
Mark Alperin <mark.t.alperin@gmail.com>
2022-01-10 16:16:05 -0600

    app.js complete with friends list and tweet form included, css styling not yet done

--- app.js
+++ app.js
@@ -6,24 +6,37 @@ $(document).ready(function(){
   $app.html('');
 
   // CREATE NEW HTML ELEMENTS
-  var $title = $('<h1>SomeName</h1>');
-  var $subtitle = $('<h2>This is a Subtitle</h2>');
+  var $title = $('<h1>Twiddler</h1>');
+  var $subtitle = $('<h2>Where the twiddlers be twiddlin\'</h2>');
   var $feed = $('<section class="container" id="feed"></section>');
   var $updateFeedButton = $('<button id="update-feed" class="button">Update Feed</button>');
+  var $friendsList = $('<section class="container" id="friendsList"></section');
+  var $newTweetForm = $('<section class="container" id="newTweetForm"></section').append(
+    $('<h3/>').text("Make a tweet!"),
+    $('<p/>').text("Username"),
+    $('<form onsubmit="return false"></form>').append($('<input/ type="text" class="form" id="formUsername" >')),
+    $('<p/>').text("Tweet"),
+    $('<form onsubmit="return false"></form>').append($('<input/ type="text" class="form" id="formMessage">'))
+  );
+  // figure out how to bind event listner to a child element
+  var $submitButton = $('<button id="makeTweet" class="button">Post Tweet</button>')
+  $submitButton.appendTo($newTweetForm);
 
   // CREATE EVENT HANDLER FUNCTIONS
-  var renderFeed = function() {
+  var renderFeed = function(whichFeed, user) {
     $feed.empty();
-    var index = streams.home.length - 1;
+    $updateFeedButton.text(user ? 'Back' : 'Update Feed');
+    var specificStream = user ? streams[whichFeed][user] : streams[whichFeed];
+    var index = specificStream.length - 1;
     while(index >= 0){
-      var tweet = streams.home[index];
+      var tweet = specificStream[index];
       var $tweet = $('<div class="tweet"></div>');
 
       var $profilePhoto = $('<img class="profile-photo"></img>')
       $profilePhoto.attr('src', tweet.profilePhotoURL);
 
       var $username = $('<div class="username"></div>');
-      $username.text(`@${tweet.user}:`);
+      $username.text(`@${tweet.user}`);
 
       var $message = $('<div class="message"></div>');
       $message.text(`${tweet.message}`);
@@ -36,8 +49,10 @@ $(document).ready(function(){
       var $share = $('<i class="fas fa-share fa-lg icon share"></i>');
       var $retweet = $('<i class="fas fa-retweet fa-lg icon retweet"></i>');
 
-      // todo: refactor icon hover listener for renderfeed's lexical scope. syntax is confusing
-      $(".icon").hover(hoverEnter, hoverExit);
+      // todo: bind event listeners for renderfeed's lexical scope and place with other eventlisteners; syntax is confusing
+      $('.icon').hover(hoverEnter, hoverExit);
+      $username.hover(hoverEnter, hoverExit);
+      $username.click(handleUsernameClick);
 
       $profilePhoto.appendTo($tweet);
       $username.appendTo($tweet);
@@ -47,37 +62,81 @@ $(document).ready(function(){
       $comment.appendTo($tweet);
       $retweet.appendTo($tweet);
       $share.appendTo($tweet);
-      $tweet.appendTo($feed);
 
+      $tweet.appendTo($feed);
       index -= 1;
     }
   };
+
+  var populatefriendsList = function() {
+    $friendsList.empty();
+    $('<h3/>').text('Friends').appendTo($friendsList);
+    for (var user in streams.users) {
+      var $friend = $(`<ul class="friend">@${user}</ul>`);
+      $friend.appendTo($friendsList);
+    }
+    // tode: same problem as with renderfeed
+    $('.friend').hover(hoverEnter, hoverExit);
+    $('.friend').click(handleUsernameClick);
+  };
+
   var handleTitleClick = function(event) {
     console.log(event);
     var titleType = event.target.localName === 'h1' ? 'title' : 'subtitle';
     alert(`The ${titleType} of this page is: ${event.target.innerText}`);
   };
+
+  var handleUsernameClick = function() {
+    var clickName = $(this).text().slice(1);
+    renderFeed('users', clickName);
+  };
+
+  var createNewTweet = function (user, message) {
+    var tweet = {};
+    tweet.user = user;
+    tweet.message = message;
+    tweet.created_at = new Date();
+    tweet.profilePhotoURL = './assets/img/visitor.png';
+    return tweet;
+  };
+
+  var handleSubmitClick = function () {
+    var user = $('#formUsername').val();
+    var message = $('#formMessage').val();
+    var isNewUser = streams.users[user] ? false : true;
+    var tweet = createNewTweet(user, message);
+    addTweet(tweet);
+    if (isNewUser) {
+      populatefriendsList();
+    }
+    renderFeed('home');
+  };
+
   var hoverEnter = function(event) {
       $(this).css('color', '#4CD2FA');
   };
   var hoverExit = function(event) {
     $(this).css('color', 'black')
-  }
-
+  };
 
   // SET EVENT LISTNERS
   $title.on("click", handleTitleClick);
   $subtitle.on("click", handleTitleClick);
-  $updateFeedButton.on("click", renderFeed);
-
-
-
+  $updateFeedButton.click(function() {
+    renderFeed('home');
+  });
+  $submitButton.click(handleSubmitClick);
 
   // APPEND NEW HTML ELEMENTS TO THE DOM
   $title.appendTo($app);
   $subtitle.appendTo($app);
   $updateFeedButton.appendTo($app);
+  $friendsList.appendTo($app);
+  $newTweetForm.appendTo($app);
   $feed.appendTo($app);
-  renderFeed();
+
+  // POPULATE INITIAL PAGE
+  populatefriendsList();
+  renderFeed('home');
 
 });
--- dataGenerator.js
+++ dataGenerator.js
@@ -16,8 +16,14 @@ window.users = Object.keys(streams.users);
 // utility function for adding tweets to our data structures
 var addTweet = function(newTweet) {
   var username = newTweet.user;
-  streams.users[username].push(newTweet);
   streams.home.push(newTweet);
+  if (streams.users[username]) {
+    streams.users[username].push(newTweet);
+  } else {
+    streams.users[username] = [];
+    streams.users[username].push(newTweet);
+  }
+
 };
 
 // utility function
