99f006031b272749314d6581e8edffe91bb9e229
Gene Wong <gene.h.wong@gmail.com>
2022-01-13 14:14:17 -0600

    Implemented a workign button that displays the correct values depending on which feed the user is on

--- app.js
+++ app.js
@@ -14,22 +14,23 @@ $(document).ready(function(){
 
 
   // Create event handler functions
-  function renderFeed(username) {
-    typeof username !== 'string' ? username = null : username = username;
-    var newIndex;
 
-    if(username) {
-      newIndex = streams.users[username].length-1
-    } else {
-        newIndex = streams.home.length-1;
-      }
+  function handleUsernameClick() {
+    var userClicked = $(this)[0].innerText.substring(1);
+    renderFeed(userClicked);
+  }
+
+  function renderFeed(user) {
+    typeof user !== 'string' ? user = null : user = user;
+    // user ? $button.text('Back') : $button.text('Update Feed')
+    var newIndex = user ? streams.users[user].length -1 : newIndex = streams.home.length-1;
     var $users = streams.users;
-    // var user = streams.user[username];
+    user ? $button.html('Back') : $button.html('Update Feed');
     $feed.html('');
 
     while(newIndex >= 0) {
       // var tweet = streams.home[newIndex];
-      var tweet = username ? streams.users[username][newIndex] : streams.home[newIndex];
+      var tweet = user ? streams.users[user][newIndex] : streams.home[newIndex];
       var $profilePhoto = $('<img class="profile-photo" />');
       var $username = $('<span class="username"></span>');
       var $message = $('<p class="message"></p>');
@@ -40,32 +41,16 @@ $(document).ready(function(){
       var $shareIcon = $('<i class="far fa-share-square fa-2x icon share"></i>');
       var $tweet = $('<div class="tweet"></div>');
 
-
-
-      /*
-
-      How to render user feed only.
-
-      //on click of a username (addEventListener for this)
-        //traverse through the list of possible users(maybe by streams.user??)
-        //if what we clicked matches one of the people in the streams.users
-          //render instead, the user with:
-            //profilePic, username, tweet, timestamp, icons + an icon to GO BACK to home stream.
-
-      */
-
       //event handlers
       $retweetIcon.on('mouseover', handleIconHover).on('mouseleave', handleIconHover);
       $commentIcon.on('mouseover', handleIconHover).on('mouseleave', handleIconHover);
       $likeIcon.on('mouseover', handleIconHover).on('mouseleave', handleIconHover);
       $shareIcon.on('mouseover', handleIconHover).on('mouseleave', handleIconHover);
 
-
       $profilePhoto.attr('src', tweet.profilePhotoURL);
       $username.text('@' + tweet.user);
       $message.text(tweet.message);
       $timestamp.text(jQuery.timeago(tweet.created_at));
-      // $tweet.text('@' + tweet.user + ': ' + tweet.message + ': ' + tweet.created_at + tweet.profilePhotoURL);
       $.each([$profilePhoto, $username, $message, $timestamp, $commentIcon, $retweetIcon, $likeIcon, $shareIcon],
         function(index,value) {
           value.appendTo($tweet);
@@ -74,10 +59,8 @@ $(document).ready(function(){
       newIndex -= 1;
     }
 
-      $($feed).find('.username').on('click', function() {
-          var userClicked = $(this)[0].innerText.substring(1);
-          renderFeed(userClicked);
-      })
+      //Additional EventHandlers
+      $($feed).find('.username').on('click', handleUsernameClick);
   }
 
 
