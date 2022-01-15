be8b0ff38b9e48ff8a557b96ef13bd2c5af61d4d
Gene Wong <gene.h.wong@gmail.com>
2022-01-14 22:26:58 -0600

    Implemented addtweet functionality which reflects into the data structures. Friends List was also implemented. CSS styling has been updated to accommodate for all changes.

--- app.js
+++ app.js
@@ -7,29 +7,118 @@ $(document).ready(function(){
   // Create new HTML elements
   $app.html('');
   var $title = $('<h1 class="title">Twiddler</h1>');
-  var $button = $('<button id="update-feed"</button>');
+  var $button = $('<button id="update-feed"></button>');
   $button.text('Update Feed');
+  var $tweetButton = $('<button id="tweetButton">Tweet</button>');
   var $feed = $('<div id=feed></div>');
+  var $sideNavContainer = $('<div class="navbar"></div>');
+  var $tweetFormContainer = $('<div class="tweetForm"></div>');
 
 
 
   // Create event handler functions
-
   function handleUsernameClick() {
     var userClicked = $(this)[0].innerText.substring(1);
     renderFeed(userClicked);
   }
 
+  function renderFriends() {
+    var $friendsDiv = $('<div class="friendsContainer"></div>')
+    var $friendsUl = $('<ul id="friends-list"></ul>');
+    var $friendsTitle = $('<h3 id="friendsTitle">Friends <i class="fas fa-user-friends"></i></h3>')
+    var friendsList = Object.keys(streams.users);
+
+    friendsList.forEach(function(friend) {
+      var $friend = $('<li class="friend"></li>');
+      $friend.text('@' + friend);
+      $friend.appendTo($friendsUl);
+    });
+
+    $button.appendTo($sideNavContainer);
+    $tweetButton.appendTo($sideNavContainer);
+    $friendsTitle.appendTo($friendsDiv);
+    $friendsUl.appendTo($friendsDiv);
+    $friendsDiv.appendTo($sideNavContainer);
+
+
+    $("#friends-list").find('li').each(function(i,li) {
+      $(li).on('click', function() {
+          var user = $(this).html().replace('@','');
+          renderFeed(user);
+      });
+    });
+  }
+
+  function renderForm() {
+    var $tweetForm = $('<form id="new-tweet-form"></form>');
+    var $submitButton = $('<button id="submit" text="Submit">Submit</button>');
+
+    $tweetForm
+      .html(`
+        <label for="username">Username:</label><br>
+        <input type="text" name="username" id="username"><br>
+        <label for="message">Write Your Message:</label><br>
+        <input type="text" id="message" name="message"><br>
+        `);
+
+    $submitButton.on('click', function(event){
+      event.preventDefault();
+
+      //check if both username and message field is filled in
+
+      var $usernameInput = $('#username').val().toLowerCase();
+      var $messageInput = $('#message').val();
+      if($usernameInput && $messageInput) {
+        //if it is, push the following data to streams.home (array of objects)
+        if (!$usernameInput.toLowerCase()) {
+          throw new Error('set the global $usernameInput.toLowerCase() property!');
+        }
+        if (!streams.users[$usernameInput]) {
+          streams.users[$usernameInput] = [];
+        }
+        var tweet = {};
+        tweet.user = $usernameInput;
+        tweet.message = $messageInput;
+        tweet.created_at = new Date();
+        tweet.profilePhotoURL = './assets/img/visitor.png';
+        addTweet(tweet);
+
+        // var newUserObj = {
+        //   user: $usernameInput,
+        //   message: $messageInput,
+        //   created_at: new Date(),
+        //   profilePhotoURL: "./assets/img/myUser.png",
+        // }
+
+        // window.streams.users[$usernameInput.toLowerCase()] === undefined ?
+        //   window.streams.users[$usernameInput.toLowerCase()] = [] : window.streams.users[$usernameInput.toLowerCase()];
+        // window.streams.users[$usernameInput.toLowerCase()].push(newUserObj);
+        // streams.home.push(newUserObj);
+
+
+        // addTweet(newUserObj);
+        //update feed with new user info??
+
+        console.log(window.streams.users)
+        $tweetFormContainer.removeClass('focus');
+        renderFeed();
+      }
+    });
+
+    //append form to app
+    $tweetForm.appendTo($tweetFormContainer);
+    $submitButton.appendTo($tweetForm)
+    $tweetFormContainer.appendTo($app);
+  }
+
   function renderFeed(user) {
-    typeof user !== 'string' ? user = null : user = user;
-    // user ? $button.text('Back') : $button.text('Update Feed')
+    typeof user !== 'string' ? user = null : user = user.toLowerCase();
     var newIndex = user ? streams.users[user].length -1 : newIndex = streams.home.length-1;
     var $users = streams.users;
     user ? $button.html('Back') : $button.html('Update Feed');
     $feed.html('');
 
     while(newIndex >= 0) {
-      // var tweet = streams.home[newIndex];
       var tweet = user ? streams.users[user][newIndex] : streams.home[newIndex];
       var $tweet = $('<div class="tweet"></div>');
       var $profilePhoto = $('<img class="profile-photo" />');
@@ -41,7 +130,6 @@ $(document).ready(function(){
       var $retweetIcon = $('<i class="fas fa-retweet fa-2x icon retweet"></i>');
       var $likeIcon = $('<i class="far fa-thumbs-up fa-2x icon like"></i>');
       var $shareIcon = $('<i class="far fa-share-square fa-2x icon share"></i>');
-
 
       //event handlers
       $retweetIcon.on('mouseover', handleIconHover).on('mouseleave', handleIconHover);
@@ -68,60 +156,35 @@ $(document).ready(function(){
       $($feed).find('.username').on('click', handleUsernameClick);
   }
 
-
-
-
   function handleIconHover() {
-    // var $empty = $myIcon.hasClass('far');
-    // var colorMatch = {
-    //   comment: 'blue',
-    //   retweet: 'green',
-    //   like: 'red',
-    //   share: 'aquablue',
-    // }
-    // function matchColor() {
-    //   for (var i = 0; i < iconSet.length; i++) {
-    //     if ($myIcon.hasClass(iconSet[i])) {
-    //       return colorMatch[iconSet[i]];
-    //     }
-    //   }
-    // }
     $(this).toggleClass('highlight');
-    // $myIcon.css('color') === $iconColor ? $myIcon.css('color', 'lightblue') : $myIcon.css('color', 'black');
-
   }
 
-
-
-
+  function showTweetForm() {
+    $tweetFormContainer.addClass('focus');
+  }
 
   // Set event listeners (providing appropriate handlers as input)
   $button.on('click', renderFeed);
-
-
-
-
+  $tweetButton.on('click', showTweetForm);
+  $(document).mouseup(function(e) {
+    if($tweetFormContainer.hasClass('focus')) {
+      if (!$tweetFormContainer.is(e.target) && $tweetFormContainer.has(e.target).length === 0){
+        $tweetFormContainer.removeClass('focus');
+      }
+    }
+  });
 
 
   // Append new HTML elements to the DOM
   $title.appendTo($app);
   $button.appendTo($app);
+  $sideNavContainer.appendTo($app);
   $feed.appendTo($app);
 
-
-
+  renderFriends();
+  renderForm();
   renderFeed();
 });
 
-
-/*
-
-How to render user feed only.
-
-//on click of a username (addEventListener for this)
-  //traverse through the list of possible users(maybe by streams.user??)
-  //if what we clicked matches one of the people in the streams.users
-    //render instead, the user with:
-      //profilePic, username, tweet, timestamp, icons + an icon to GO BACK to home stream.
-
-*/
+window.isItBeautifulYet = true;
---
+++ extraCredit.md
@@ -0,0 +1,15 @@
+User Interface (Extra Credit)
+.A Friends List, with a list view of each of the user's friends' usernames.
+.A New Tweet Form, complete with a:
+    .Username Input Label
+    .Username Input Field
+    .Message Input Label
+    .Message Input Field
+    .Submit Button
+
+User Experience (Extra Credit).
+.Clicking a friend's username inside the Friends List to re-render the Feed with the most current list of that specific user's Tweets.
+.Typing their own Username and Tweet Message into the Tweet Form, and then clicking the Submit Button to create a new Tweet, which should:
+.Create and store a new Tweet object in the appropriate streams of the Back End code.
+.Immediately update the Home Feed.
+.Immediately update the Friends List, if and only if this user has never tweeted before.
