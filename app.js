b6ba815773944b851d105a9b07102e22d4380e49
Lauren <laurendayle.anderson@gmail.com>
2022-01-15 17:49:07 -0500

    bare minimum requirements complete

--- app.js
+++ app.js
@@ -1,100 +1,3 @@
-// $(document).ready(function(){
-
-// // Select already existing elements
-//   var $app = $('#app');
-//   var $usernames = $('.username');
-
-// // Create new HTML elements
-
-//   var $title = $('<h1>Twiddler</h1>');
-
-//   var $updateFeedButton = $('<button id="update-feed">Update Feed</button>');
-
-//   var feedDiv = $('#feed');
-//   var $feedDiv = $('<div id="feed"></div>');
-
-// // Create event handler functions
-//   jQuery('.timeago').timeago();
-//     $('.timestamp').on('click', function() {
-//     $('.timeago').timeago('update', new Date());
-//   });
-//   jQuery.timeago.settings.allowFuture = true;
-
-//   // render feed with tweets in appropriate format
-//   var renderFeed = function(user) {
-//     $feedDiv.text('');
-//     var index = streams.home.length - 1;
-
-//     while (index >= 0) {
-//       var tweet = streams.home[index];
-//       var $tweet = $('<div class="tweet"></div>');
-//       $tweet.appendTo('#feed');
-
-//       var $tweetImg = $('<img class="profile-photo">');
-//       var profilePhotos = {
-//         'douglascalhoun': '/Users/laurenanderson/Documents/Hack Reactor - Precourse/seip2202-twiddler/assets/img/douglascalhoun.png',
-//         'mracus': '/Users/laurenanderson/Documents/Hack Reactor - Precourse/seip2202-twiddler/assets/img/mracus.png',
-//         'sharksforcheap': '/Users/laurenanderson/Documents/Hack Reactor - Precourse/seip2202-twiddler/assets/img/sharksforcheap.png',
-//         'shawndrost': '/Users/laurenanderson/Documents/Hack Reactor - Precourse/seip2202-twiddler/assets/img/shawndrost.png',
-//         'visitor': '/Users/laurenanderson/Documents/Hack Reactor - Precourse/seip2202-twiddler/assets/img/visitor.png'
-//       };
-//       var imgSrc = profilePhotos[tweet.user];
-//       $tweetImg.attr('src', imgSrc);
-//       $tweet.append($tweetImg);
-
-//       var $user = $('<span class="username"></span>');
-//       $tweet.append($user);
-//       $user.text('@' + tweet.user);
-
-//       var $message = $('<p class="message"></p>');
-//       $tweet.append($message);
-//       $message.text(tweet.message);
-
-//       var $timestamp = $('<time class="timestamp timeago"></time>');
-//       $tweet.append($timestamp);
-//       $timestamp.text(jQuery.timeago(new Date()));
-
-//       var $commentIcon = $('<i id="comment-icon" class="icon comment fas fa-comment"></i>');
-//       $tweet.append($commentIcon);
-
-//       var $retweetIcon = $('<i id="retweet-icon" class="icon retweet fas fa-retweet"></i>');
-//       $tweet.append($retweetIcon);
-
-//       var $likeIcon = $('<i id="like-icon" class="icon like fas fa-heart"></i>');
-//       $tweet.append($likeIcon);
-
-//       var $shareIcon = $('<i id="share-icon" class="icon share fas fa-share"></i>');
-//       $tweet.append($shareIcon);
-
-//       index--;
-//     }
-//   };
-
-//   renderFeed();
-
-//   var handleUsernameClick = $usernames.on('click', function() {
-//     if ($updateFeedButton.text() === 'Update Feed') {
-//       $updateFeedButton.text('Back');
-//     }
-//   });
-
-// // Set event listeners (providing appropriate handlers as input)
-//   var $icons = $('.icon');
-//   $icons.hover(function() {
-//     $(this).css('color', 'lightgray');
-//   }, function() {
-//     $(this).css('color', 'black');
-//   });
-
-//   $updateFeedButton.on('click', renderFeed);
-
-// // Append new HTML elements to the DOM
-//   $title.appendTo($app);
-//   $feedDiv.appendTo($app);
-//   $updateFeedButton.insertAfter($title);
-
-//   });
-
 $(document).ready(function() {
 
   // create and append app div
@@ -160,7 +63,7 @@ $(document).ready(function() {
 
       var $timestamp = $('<time class="timestamp timeago"></time>');
       $tweet.append($timestamp);
-      $timestamp.text(jQuery.timeago(new Date()));
+      $timestamp.text(jQuery.timeago(tweet.created_at));
 
       var $commentIcon = $('<i id="comment-icon" class="icon comment fas fa-comment"></i>');
       $tweet.append($commentIcon);
@@ -194,12 +97,13 @@ $(document).ready(function() {
   // add hover event to change icon color
   var $icons = $('.icon');
   $(document).on('mouseenter', '.icon', function() {
-    $(this).css('color', 'lightgray');
+    $(this).css('color', 'lightblue');
   });
   $(document).on('mouseleave', '.icon', function() {
     $(this).css('color', 'black');
   });
 
+  // add click event to switch from home feed to user feed and back to home feed
   var $allTweets = $('.tweet');
   var $usernames = $('.username');
   var handleUsernameClick = $(document).on('click', '.username', function(event) {
@@ -210,5 +114,5 @@ $(document).ready(function() {
     }
   });
 
-
+  window.isItBeautifulYet = true;
 });
