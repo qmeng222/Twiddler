559dc87f6a45ebe6902b5e389f831989ffbf5515
Sean McNamara <seanmcnamara33@gmail.com>
2022-01-15 18:37:57 -0700

    refactor so on-click event for 'update-feed' button passes renderFeed a 'user' value, which is then used to determine whether or not to show home feed or user feed.

--- app.js
+++ app.js
@@ -8,85 +8,73 @@ $(document).ready(function(){
 
   // create update feed button
   var $updateFeed = $('<button id="update-feed">Update Feed</button>');
-  $updateFeed.appendTo($app);
+  var $btnSpan = $('<span id="btn-span"></span');
+  $($btnSpan).append($updateFeed);
+  $btnSpan.appendTo($app);
 
   // create feed container
   var $feed = $('<div id="feed"></div>')
   $feed.appendTo($app);
 
   // render feed utility function (event handler function)
-  var renderFeed = function() {
+  var renderFeed = function(user = 'home') {
     // remove tweets from feed
     $feed.html('');
-    // console.log(event);
     // for each tweet object in the stream array (in reverse order)
     var index;
     // create functionality for individual pages of tweets
-    if (event.target.outerText === 'Update Feed' || event.target.outerText === 'Back' || event.target === document) {
-      $updateFeed.text('Update Feed');
-      index = streams.home.length - 1;
+    if (user === 'home') {
+      index = streams[user].length - 1;
       while(index >= 0){
         var tweet = streams.home[index];
-        // console.log(tweet);
-        //create a new Tweet UI component
         var $tweet = $('<div class="tweet"></div>');
         var profilePic = tweet.profilePhotoURL;
         var $profilePhoto = $('<img class="profile-photo">');
-        $($profilePhoto).attr('src', profilePic);
         var $userName = $('<div class="username"></div>');
-        $($userName).html('@' + tweet.user);
+        var $header = $('<div id="tweetHeader"></div>');
+        $profilePhoto.appendTo($header);
+        $userName.appendTo($header)
         var $message = $('<p class="message"></p>');
-        $($message).html(tweet.message);
         var $timeStamp = $('<div class="timestamp"></div>');
-        $($timeStamp).html(jQuery.timeago(tweet.created_at));
         var $comment = $('<i class="icon comment far fa-comment-alt"></i>')
         var $retweet = $('<i class="icon retweet fas fa-retweet"></i>');
         var $like = $('<i class="icon like far fa-heart"></i>');
         var $share = $('<i class="icon share far fa-share-square"></i>');
-        $profilePhoto.appendTo($tweet);
-        $userName.appendTo($tweet);
-        $message.appendTo($tweet);
-        $timeStamp.appendTo($tweet);
-        $comment.appendTo($tweet);
-        $retweet.appendTo($tweet);
-        $like.appendTo($tweet);
-        $share.appendTo($tweet);
+        var components = [$tweet, $header, $profilePhoto, $userName, $message, $timeStamp, $comment, $retweet, $like, $share];
+        $($profilePhoto).attr('src', profilePic);
+        $($userName).html('@' + tweet.user);
+        $($message).html(tweet.message);
+        $($timeStamp).html(jQuery.timeago(tweet.created_at));
+        components.forEach(function(el, index) {
+          $(el).appendTo($tweet);
+        })
         //append the new Tweet UIC component to the feed
         $tweet.appendTo($feed);
         index -= 1;
       }
     }
     else {
-      $updateFeed.text('Back');
-      var user = event.target.innerHTML.slice(1);
       index = streams.users[user].length - 1;
-      console.log(streams.users[user]);
       while(index >= 0){
         var tweet = streams.users[user][index];
-        // console.log(tweet);
-        //create a new Tweet UI component
         var $tweet = $('<div class="tweet"></div>');
         var profilePic = tweet.profilePhotoURL;
         var $profilePhoto = $('<img class="profile-photo">');
-        $($profilePhoto).attr('src', profilePic);
         var $userName = $('<div class="username"></div>');
-        $($userName).html('@' + tweet.user);
         var $message = $('<p class="message"></p>');
-        $($message).html(tweet.message);
         var $timeStamp = $('<div class="timestamp"></div>');
-        $($timeStamp).html(jQuery.timeago(tweet.created_at));
         var $comment = $('<i class="icon comment far fa-comment-alt"></i>')
         var $retweet = $('<i class="icon retweet fas fa-retweet"></i>');
         var $like = $('<i class="icon like far fa-heart"></i>');
         var $share = $('<i class="icon share far fa-share-square"></i>');
-        $profilePhoto.appendTo($tweet);
-        $userName.appendTo($tweet);
-        $message.appendTo($tweet);
-        $timeStamp.appendTo($tweet);
-        $comment.appendTo($tweet);
-        $retweet.appendTo($tweet);
-        $like.appendTo($tweet);
-        $share.appendTo($tweet);
+        var components = [$tweet, $profilePhoto, $userName, $message, $timeStamp, $comment, $retweet, $like, $share];
+        $($profilePhoto).attr('src', profilePic);
+        $($userName).html('@' + tweet.user);
+        $($message).html(tweet.message);
+        $($timeStamp).html(jQuery.timeago(tweet.created_at));
+        components.forEach(function(el, index) {
+          $(el).appendTo($tweet);
+        })
         //append the new Tweet UIC component to the feed
         $tweet.appendTo($feed);
         index -= 1;
@@ -94,12 +82,20 @@ $(document).ready(function(){
     }
   }
 
+  // helper function for username click
+  var handleUsernameClick = function() {
+    $updateFeed.text('Back');
+    renderFeed(this.innerHTML.slice(1));
+  }
   // update feed when button clicked
-  $updateFeed.on("click", renderFeed);
+  $updateFeed.on("click", function() {
+    $updateFeed.text('Update Feed');
+    renderFeed('home');
+  });
 
   // populate feed on page load
   renderFeed();
 
   // update to individual feed when username clicked
-  $feed.on("click", ".username", renderFeed);
+  $feed.on("click", 'div.username', handleUsernameClick);
 });
