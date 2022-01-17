12dbd1b6f3b8882b76613979416092eaace4d526
Sean McNamara <seanmcnamara33@gmail.com>
2022-01-17 11:46:18 -0700

    refactor by grouping variables, functions, and function calls together. Refactor functions themselves for better readability, refactor variable names and class/id names to better reflect content in app.js

--- app.js
+++ app.js
@@ -1,142 +1,154 @@
 $(document).ready(function(){
+  // create jQuery variable for #app div (div were building everyting in) and clear anything inside
   var $app = $('#app');
   $app.html('');
 
-  // create twiddler title
+  // create twiddler title jQuery var and add to app variable
   var $title = $('<h1>Twiddler</h1>');
   $title.appendTo($app);
 
-  // create update feed button
-  var $updateFeed = $('<button id="update-feed">Update Feed</button>');
+  // create post variables
+  var $createPost = $('<div id="tweet-form"></div>');
+  var $form = $('<form target="" id="new-tweet-form"></form>');
+  var $yourUser = $('<input name="username" type="text" id="username" class="input" placeholder="Username"></input>');
+  var $userLabel = $('<label for="username"></label');
+  // needed to use <inpu> for automated test, but would prefer <textarea> here
+  var $tweetContent = $('<input name="message" type="text" id="message" class="input" placeholder="Enter Tweet Here"></input>');
+  var $tweetContentLabel = $('<label for="message"></label>');
+  var $postButton = $('<button type="submit" class="btn">Tweet!</button>');
+  // add all create post variables to their respective jQuery variables
+  $form.append($yourUser, $userLabel, $tweetContent, $tweetContentLabel, $postButton);
+  $form.appendTo($createPost);
+  $createPost.appendTo($app);
+
+  // create update feed button in a container (to make formatting easier) and add it to app
+  var $updateFeed = $('<button class="btn" id="update-feed">Update Feed</button>');
   var $btnSpan = $('<span id="btn-span"></span');
-  $($btnSpan).append($updateFeed);
+  $btnSpan.append($updateFeed);
   $btnSpan.appendTo($app);
 
-  // main page
-  var $mainPage = $('<div id="main"></div>')
-  $mainPage.appendTo($app);
-  // sidebar container
+  // create contianers for main, sidebar, and feed
+  var $main = $('<div id="main"></div>')
   var $sideBar = $('<div id="sidebar"><strong>Friends List</strong></div>');
-  $sideBar.appendTo($mainPage);
-  // create feed container
   var $feed = $('<div id="feed"></div>')
-  $feed.appendTo($app);
+  $main.append($sideBar, $feed);
+  $main.appendTo($app);
+
+  // create tweet function
+  var createTweet = function(index, target) {
+    // javascript variables
+    var tweet = target[index];
+    var profilePic = tweet.profilePhotoURL;
+    // jquery variables
+    var $tweet = $('<div class="tweet"></div>');
+    var $profilePhoto = $('<img class="profile-photo">');
+    var $userName = $('<div class="username"></div>');
+    var $timeStamp = $('<div class="timestamp"></div>');
+    var $content = $('<p class="content message"></p>');
+    var $comment = $('<i class="icon comment far fa-comment-alt"></i>')
+    var $retweet = $('<i class="icon retweet fas fa-retweet"></i>');
+    var $like = $('<i class="icon like far fa-heart"></i>');
+    var $share = $('<i class="icon share far fa-share-square"></i>');
+    // jquery variables for formatting tweet container
+    var $header = $('<div id="tweet-header"></div>');
+    var $body = $('<div class="body"></body>');
+    var $footer = $('<footer class="footer"></footer>');
+    var components = [$header, $body, $footer];
+    // add variables to their respective containers
+    $header.append($profilePhoto, $userName, $timeStamp);
+    $body.append($content);
+    $footer.append($comment, $retweet, $like, $share);
+    // set attributes/text of create tweet variables equal to data in users(tweeters) object
+    $($profilePhoto).attr('src', profilePic);
+    $($userName).html('@' + tweet.user);
+    $($content).html(tweet.message);
+    // use timeago library to make date tag 'human readable'
+    $($timeStamp).html(jQuery.timeago(tweet.created_at));
+    // loop over compenents array and add each element to $tweet jQuery variable
+    components.forEach(function(el, index) {
+      $(el).appendTo($tweet);
+    })
+    // return formatted tweet
+    return $tweet;
+  }
 
   // render feed utility function (event handler function)
   var renderFeed = function(user = 'home') {
     // remove tweets from feed
     $feed.html('');
-    // for each tweet object in the stream array (in reverse order)
     var index;
+    var target;
     // create functionality for individual pages of tweets
+    // set index and target based on value of user in function input
     if (user === 'home') {
       index = streams[user].length - 1;
-      while(index >= 0){
-        var tweet = streams.home[index];
-        var $tweet = $('<div class="tweet"></div>');
-        var profilePic = tweet.profilePhotoURL;
-        var $profilePhoto = $('<img class="profile-photo">');
-        var $userName = $('<div class="username"></div>');
-        var $timeStamp = $('<div class="timestamp"></div>');
-        var $header = $('<div id="tweetHeader"></div>');
-        $profilePhoto.appendTo($header);
-        $userName.appendTo($header)
-        $timeStamp.appendTo($header);
-        var $message = $('<p class="message"></p>');
-        var $body = $('<div class="body"></body>');
-        $message.appendTo($body);
-        var $comment = $('<i class="icon comment far fa-comment-alt"></i>')
-        var $retweet = $('<i class="icon retweet fas fa-retweet"></i>');
-        var $like = $('<i class="icon like far fa-heart"></i>');
-        var $share = $('<i class="icon share far fa-share-square"></i>');
-        var $footer = $('<footer class="footer"></footer>');
-        $comment.appendTo($footer);
-        $retweet.appendTo($footer);
-        $like.appendTo($footer);
-        $share.appendTo($footer);
-        var components = [$header, $body, $footer];
-        $($profilePhoto).attr('src', profilePic);
-        $($userName).html('@' + tweet.user);
-        $($message).html(tweet.message);
-        $($timeStamp).html(jQuery.timeago(tweet.created_at));
-        components.forEach(function(el, index) {
-          $(el).appendTo($tweet);
-        })
-        //append the new Tweet UIC component to the feed
-        $tweet.appendTo($feed);
-        index -= 1;
-      }
+      target = streams.home;
     }
     else {
       index = streams.users[user].length - 1;
-      while(index >= 0){
-        var tweet = streams.users[user][index];
-        var $tweet = $('<div class="tweet"></div>');
-        var profilePic = tweet.profilePhotoURL;
-        var $profilePhoto = $('<img class="profile-photo">');
-        var $userName = $('<div class="username"></div>');
-        var $timeStamp = $('<div class="timestamp"></div>');
-        var $header = $('<div id="tweetHeader"></div>');
-        $profilePhoto.appendTo($header);
-        $userName.appendTo($header)
-        $timeStamp.appendTo($header);
-        var $message = $('<p class="message"></p>');
-        var $body = $('<div class="body"></body>');
-        $message.appendTo($body);
-        var $comment = $('<i class="icon comment far fa-comment-alt"></i>');
-        var $retweet = $('<i class="icon retweet fas fa-retweet"></i>');
-        var $like = $('<i class="icon like far fa-heart"></i>');
-        var $share = $('<i class="icon share far fa-share-square"></i>');
-        var $footer = $('<footer class="footer"></footer>');
-        $comment.appendTo($footer);
-        $retweet.appendTo($footer);
-        $like.appendTo($footer);
-        $share.appendTo($footer);
-        var components = [$header, $body, $footer];
-        $($profilePhoto).attr('src', profilePic);
-        $($userName).html('@' + tweet.user);
-        $($message).html(tweet.message);
-        $($timeStamp).html(jQuery.timeago(tweet.created_at));
-        components.forEach(function(el, index) {
-          $(el).appendTo($tweet);
-        })
-        //append the new Tweet UIC component to the feed
-        $tweet.appendTo($feed);
-        index -= 1;
-      }
+      target = streams.users[user];
     }
-    $feed.appendTo($mainPage);
+    // loop tweet object in the index (in reverse order)
+    while(index >= 0){
+      // call createTweet with index and target, and append the result to $feed, then decrement
+      var $tweet = createTweet(index, target);
+      $tweet.appendTo($feed);
+      index -= 1;
+    }
+    // add feed to $main container
+    $feed.appendTo($main);
   }
-
-  // friends list
-  // function for friends list
-  var $friendsList = $('<ul class="friends-list" aria-label="Friends List"></ul>');
-  for (var user in streams.users) {
-    user = "@" + user;
-    var $liUser = $('<li class="friend"></li>');
-    $liUser.text(user);
-    $liUser.appendTo($friendsList);
-  }
-  $friendsList.appendTo($sideBar);
-
 
   // helper function for username click
   var handleUsernameClick = function() {
     $updateFeed.text('Back');
     renderFeed(this.innerHTML.slice(1));
   }
+
+  // friends list unordered list
+  var $friendsList = $('<ul id="friends-list" aria-label="Friends List"></ul>');
+  // loop over streams.users
+  for (var user in streams.users) {
+    // add @ at beginning of user text
+    user = '@' + user;
+    // create an li for each user, add the 'user' variable to the li, add li to friendslist
+    var $liUser = $('<li class="friend"></li>');
+    $liUser.text(user);
+    $liUser.appendTo($friendsList);
+  }
+  // add friendsList to the sidebar container
+  $friendsList.appendTo($sideBar);
+
+  // helper function for username click
+  var handleUsernameClick = function() {
+    $updateFeed.text('Back');
+    renderFeed(this.innerHTML.slice(1));
+  }
+  // Event Handlers
+  // create tweet submit button click
+  $postButton.on('click', function(event) {
+    // prevent page from refreshing
+    event.preventDefault();
+    // stringify value in $yourUser(username) and $tweetContent(message) and set to variable
+    var name = ($yourUser.val()).toString();
+    var tweetContent = ($tweetContent.val()).toString();
+    // call writeTweet function from dataGenerator.js and pass in message and name
+    writeTweet(tweetContent, name);
+    // call renderFeed function with defaul argument
+    renderFeed();
+  })
   // update feed when button clicked
-  $updateFeed.on("click", function() {
+  $updateFeed.on('click', function() {
     $updateFeed.text('Update Feed');
     renderFeed('home');
   });
+  // update to individual feed when username or sidebar clicked
+  $feed.on('click', 'div.username', handleUsernameClick);
+  $sideBar.on('click', '.friend', handleUsernameClick);
 
   // populate feed on page load
   renderFeed();
 
-  // update to individual feed when username clicked
-  $feed.on("click", 'div.username', handleUsernameClick);
-
-  $sideBar.on("click", '.user', handleUsernameClick);
+  window.isItBeautifulYet = true;
 });
 
