c4cd1b3ac62551a3bebce9f7151e50cdd35f55f2
Leia Harlow <leiaharlow@Leias-MacBook-Pro.local>
2022-01-20 17:27:05 -0800

    Set up app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  var $app = $('#app');
+  var $title = $('<h1>Twiddler</h1>');
+  $app.html('');
+  $title.appendTo($app);
+  $title.on("click", function(event) {
+    console.log(event);
+    alert('The title of this page is: ' + event.target.innerText);
+});
+
+  var index = streams.home.length - 1;
+  while(index >= 0){
+    var tweet = streams.home[index];
+    var $tweet = $('<div class="tweet"></div>');
+    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    $tweet.appendTo($app);
+    index -= 1;
+  }
+
+});
