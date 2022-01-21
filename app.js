88983137819c24a141b1915242dd74944985c285
Leia Harlow <leiaharlow@Leias-MacBook-Pro.local>
2022-01-20 18:54:16 -0800

    Start icon section

--- app.js
+++ app.js
@@ -8,13 +8,40 @@ $(document).ready(function(){
     alert('The title of this page is: ' + event.target.innerText);
 });
 
+function renderFeed(user) {
+  //$feed.html(''); // .empty?
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
     var $tweet = $('<div class="tweet"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($app);
+    var $message = $('<div class="message">' + tweet.message + '</div>');
+    var $username = $('<div class="username">' + '@' + tweet.user + '</div>');
+    var $img = $('<img class="profile-photo"></img>');
+    var $timestamp = $('<div class="timestamp">' + tweet.created_at + '</div>');
+    var $comment = $('<img class="comment" src="assets/icons/placeholder.png"></img>');
+    $message.appendTo($tweet);
+    $username.appendTo($tweet);
+    $img.appendTo($tweet);
+    $timestamp.appendTo($tweet);
+    $comment.appendTo($tweet);
+    $tweet.appendTo($feed);// app
     index -= 1;
-  }
+  } // link tweets to log 11
+};
+
+var $button = $('<button id="update-feed">Update Feed</button>');
+$button.appendTo($app);
+$button.click("#update-feed", function (event) {
+  $feed.empty();
+  renderFeed();
 
 });
+
+
+  var $feed = $('<div id="feed"></div>');
+  $feed.appendTo($app);
+
+  renderFeed();
+
+});
+
