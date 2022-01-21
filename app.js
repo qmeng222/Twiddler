b888d94949d7fcb6baebf31606a9c8c12a85c708
Othniel Chan <othnielhr.chan@gmail.com>
2022-01-21 17:01:19 -0600

    skeleton

---
+++ app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  var $app = $('#app');
+  $app.html('');
+
+  function update() {
+    $('#update-feed').text('Update Feed')
+    render();
+  }
+
+  $("button").on("click", update);
+
+  var render = function(user) {
+    $app.empty();
+    var index = streams.home.length - 1;
+    while(index >= 0){
+      var tweet = streams.home[index];
+      var $tweet = $('<div class="tweet"></div>');
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      $tweet.appendTo($app);
+      index -= 1;
+    }
+  }
+  render();
+
+});
