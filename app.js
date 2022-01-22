22702a5833c70acc3cad878ae8fcaebd664593ec
Othniel Chan <othnielhr.chan@gmail.com>
2022-01-21 21:28:19 -0600

    complete render of messages and update

--- app.js
+++ app.js
@@ -2,24 +2,43 @@ $(document).ready(function(){
   var $app = $('#app');
   $app.html('');
 
+  var $title = $('<h1 id="title">Twiddler</h1>');
+  var $updatefeed = $('<button id="update-feed">Update Feed</button>');
+  var $feed = $('<div id="feed"></div>');
+
   function update() {
-    $('#update-feed').text('Update Feed')
+    $('#update-feed').text('Update Feed');
     render();
   }
 
-  $("button").on("click", update);
+  function user(user) {
+    $('#update-feed').text('Back');
+    render(user.data);
+  }
+
+  $updatefeed.on("click", update);
+
+  $title.appendTo($app);
+  $updatefeed.appendTo($app);
+  $feed.appendTo($app);
 
   var render = function(user) {
-    $app.empty();
-    var index = streams.home.length - 1;
+    $feed.empty();
+    var data = [];
+    if (user) {
+      alert("clicked user");
+      data = streams.users[user];
+    } else {
+      data = streams.home;
+    }
+    var index = data.length - 1;
     while(index >= 0){
-      var tweet = streams.home[index];
+      var tweet = data[index];
       var $tweet = $('<div class="tweet"></div>');
       $tweet.text('@' + tweet.user + ': ' + tweet.message);
-      $tweet.appendTo($app);
+      $tweet.appendTo($feed);
       index -= 1;
     }
   }
   render();
-
 });
