4c64e3300dd17d9fca2634f247d714b66196a5ab
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-01-22 09:14:11 -0500

    Still struggling on twiddler, yay

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 $(document).ready(function(){
+
   var $app = $('#app');
   $app.html('');
-  var $title = $('<h1>Twiddler</h1>');
-  var $refreshFeed = $('<div id="update-feed">Update Feed</div>');
-  var $feed = $('<div id-"feed"></div>');
-  $title.appendTo($app);
-  $refreshFeed.appendTo($feed);
-  $feed.appendTo($app);
 
-  $title.on("click", function(event) {
-    console.log(event);
-    alert("The title of this page is: " + event.target.innerText);
-  });
+  var $title = $('<h1>Twiddler</h1>'); // create title h element
+    $title.appendTo($app); // append title
 
-  var tweetGenerator = function() {
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div class="tweet"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($feed);
-    index -= 1;
+  var $refreshFeed = $('<div id="update-feed">Update Feed</div>'); // create refresh button div
+    $refreshFeed.appendTo($app); // append refresh button
+
+  var $feed = $('<div class="feed"></div>'); // create feed div, class feed
+    $feed.appendTo($app); // append feed element
+
+  // streams.home holds array of tweet data objects
+    // tweets added based on timing since last refresh
+      // initially generates 10 tweets, generates a new tweet per second or so after page load
+        // tweets should be more or less unique at time of generation
+
+  // if tweets are pushed to streams.home over time, newest tweet are on the end of the array
+    // if everything is un-appended on button click
+      // re-append every tweet in reverse
+  var streamArray = streams.home; // aliased array of tweet objects
+
+  var tweetWriter = function() {
+
+  for (var x = streamArray.length - 1; x >= 0; x--) {  // appending array objects backwards
+    var tweetData = streamArray[x];  // individual tweet data
+    var $tweet = $('<div class="tweet"></div>'); // represent parking spot for actual visual tweet data
+    var message = $tweet.text('@' + tweetData.user + ': ' + tweetData.message); // assembled tweet
+    message.appendTo($feed);
   }
-}
-$refreshFeed.click(tweetGenerator);
+  // console.log(window.streams, streams.home);
 
+}
+$refreshFeed.click(tweetWriter);
 });
