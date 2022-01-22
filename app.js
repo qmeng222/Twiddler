b7097f799a6ba8c68aa85e8b9418ee65b3c321af
Michael Manzo <mpmanzo@gmail.com>
2022-01-22 14:42:41 -0700

    implement Tweet UI components

--- app.js
+++ app.js
@@ -1,51 +1,90 @@
 $(document).ready(function(){
-  // Select the div with the ID #app
-  var $app = $('#app');
 
-  // Reset the html to ''
+  // ----------------------------------------------------------------------------------------------
+  // Select already existing elements
+  // ----------------------------------------------------------------------------------------------
+
+  var $body = $('body');
+  var $app = $('#app');
   $app.html('');
 
-  // Create an h1 element with the text "twiddler"
+  // ----------------------------------------------------------------------------------------------
+  // Create new HTML elements
+  // ----------------------------------------------------------------------------------------------
+
   var $title = $('<h1>Twiddler</h1>');
+  var $subtitle = $('<h2></h2>');
+  var $updateButton = $('<button id="update-feed">Update Button</button>');
+  var $feed = $('<div id="feed"></div>');
+  var $tweet
 
-  // Append the h1 element to the DOM, nested inside of the #app div
-  $title.appendTo($app);
+  // ----------------------------------------------------------------------------------------------
+  // Create main functions
+  // ----------------------------------------------------------------------------------------------
 
-  // Set a click event listener on the h1 element
-  $title.on("click", function(event) {
-    console.log(event);
-    alert('The title of this page is: ' + event.target.innerText);
-  });
-
-  // Create a button with the ID "update-feed"
-  var $updateBtn = $('<button id="update-feed">Update Feed</button>');
-
-  // Append the button to the DOM, nested inside of the #app div
-  $updateBtn.appendTo($app);
-
-  // Create a div with the ID "feed"
-  var $feed = $('<div id="feed"></div>')
-
-  // Append the div#feed to the DOM, nested inside the #app div
-  $feed.appendTo($app);
-
-  // Add a click event listener on the #update-feed button
-  $updateBtn.on("click", function(event) {
-    updateFeed();
-  })
-
-  var updateFeed = function() {
+  var renderFeed = function() {
     var index = streams.home.length - 1;
     $(".tweet").remove();
     while(index >= 0){
       var tweet = streams.home[index];
-      var $tweet = $('<div class="tweet"></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      // var $tweet = $('<div class="tweet"></div>');
+      // $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      var username = '@' + tweet.user;
+      var $tweet = renderTweet(tweet.profilePhotoURL, username, tweet.message, tweet.created_at);
       $tweet.appendTo($feed);
       index -= 1;
     }
   };
 
-  updateFeed();
+  var renderTweet = function(profilePhoto, username, message, timestamp) {
+    var $tweet = $('<div class="tweet"></div>');
+    var $profilePhoto = $('<img class="profile-photo" src=' + profilePhoto + ' alt="no profile photo">');
+    var $username = $('<span class="username">' + username + '</span>');
+    var $message = $('<p class="message">' + message + '</p>');
+    var $timestamp = $('<span class="timestamp">' + timestamp + '</span>');
+    var $icons = $('<div class="icon"></div>');
+    var $commentIcon = $('<img class="icon comment" src="assets/icons/placeholder.png" alt="comment">');
+    var $retweetIcon = $('<img class="icon retweet" src="assets/icons/placeholder.png" alt="retweet">');
+    var $likeIcon = $('<img class="icon like" src="assets/icons/placeholder.png" alt="like">');
+    var $shareIcon = $('<img class="icon share" src="assets/icons/placeholder.png" alt="share">');
+    $icons.append($commentIcon, $retweetIcon, $likeIcon, $shareIcon);
+    $tweet.append($profilePhoto, $username, $message, $timestamp, $icons);
+    return $tweet;
+  };
+
+  // ----------------------------------------------------------------------------------------------
+  // Create event handler functions
+  // ----------------------------------------------------------------------------------------------
+
+  var handleTitleClick = function(event) {
+    var titleType = event.target.localName === 'h1' ? 'title' : 'subtitle';
+    alert('The ' + titleType + ' of this page is: ' + event.target.innerText);
+  };
+
+  var handleUpdateButtonClick = function(event) {
+    renderFeed();
+  };
+
+  // ----------------------------------------------------------------------------------------------
+  // Set event listeners (providing appropriate handlers as input)
+  // ----------------------------------------------------------------------------------------------
+
+  $title.on("click", handleTitleClick);
+  $updateButton.on("click", handleUpdateButtonClick);
+
+  // ----------------------------------------------------------------------------------------------
+  // Append new HTML elements to the DOM
+  // ----------------------------------------------------------------------------------------------
+
+  $title.appendTo($app);
+  $subtitle.appendTo($app);
+  $updateButton.appendTo($app);
+  $feed.appendTo($app);
+
+  // ----------------------------------------------------------------------------------------------
+  // Misc
+  // ----------------------------------------------------------------------------------------------
+
+  renderFeed();
 
 });
