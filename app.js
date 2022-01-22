0e72e008a94571932648d601bda7bd6957723107
Othniel Chan <othnielhr.chan@gmail.com>
2022-01-22 17:15:08 -0600

    added toggle for icons

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ $(document).ready(function(){
     render(user.data);
   }
 
+  function clickAdv($this) {
+    // console.log(this);
+    $(this).toggleClass("far fas");
+  }
+
   $updatefeed.on("click", update);
 
   $title.appendTo($app);
@@ -36,6 +41,10 @@ $(document).ready(function(){
       var $tweet = $('<div class="tweet"></div>');
       var $user = $('<div class="user"></div>');
       var $msg = $('<div class="msg"></div>');
+      var $comment = $('<i id="com" class="far fa-comments"></i>');
+      var $retweet = $('<i class="fas fa-retweet"></i>');
+      var $like = $('<i class="far fa-heart"></i>');
+      var $share = $('<i class="far fa-share-square"></i>');
       var tweet = data[index];
       $msg.text(tweet.message);
       $user.text('@' + tweet.user);
@@ -43,7 +52,14 @@ $(document).ready(function(){
       $tweet.appendTo($feed);
       $user.appendTo($tweet);
       $msg.appendTo($tweet);
+      $comment.appendTo($tweet);
+      $retweet.appendTo($tweet);
+      $like.appendTo($tweet);
+      $share.appendTo($tweet);
       $user.click(tweet.user, userClick);
+      $comment.click(clickAdv);
+      $like.click(clickAdv);
+      $share.click(clickAdv);
       index -= 1;
     }
   }
