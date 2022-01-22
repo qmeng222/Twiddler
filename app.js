a621e3a5d8841d3b90af0a65df2a5cc55c67ebad
James <jamesoh97@gmail.com>
2022-01-21 16:48:26 -0800

    selecting usernames under friends list redirects to user feed

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ $(document).ready(function () {
   var $home = $('#home');
   var $update = $('#update-feed');
   var $form = $('.tweet-form');
-  var $back = $('#back');
-  var $twiddlers = $('.feed-container');
-  $back.hide();
 
   var updateFeed = function (selectedUser) {
     var index = selectedUser ? streams.users[selectedUser].length - 1 : streams.home.length - 1;
@@ -15,9 +12,9 @@ $(document).ready(function () {
       var $tweet = $('<div class="tweet"></div>');
       var $img = $('<img class="profile-photo" src="assets/img/' + tweet.user + '.png" alt="User Profile Picture"/>');
       var $twiddlerContainer = $('<div class="twiddler-container"></div>');
-      var $username = $('<div class="username" id="' + tweet.user + '">' + '@' + tweet.user + '</div>');
+      var $username = $('<div class="username ' + tweet.user + '">' + '@' + tweet.user + '</div>');
       var $message = $('<div class="message">' + tweet.message + '</div>');
-      var $interactions = $('<div class="interactions"><i class="fas fa-comment comment"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i><i class="fas fa-share"></i></div>');
+      var $interactions = $('<div class="interactions"><i class="fas fa-comment comment"></i><i class="fas fa-retweet retweet"></i><i class="fas fa-heart like"></i><i class="fas fa-share share"></i></div>');
       var $timeStamp = $('<time class="timeago timestamp" datetime=' + tweet.created_at.toISOString() + '></time>');
 
       $twiddlerContainer.append($username).append($message).append($interactions).append($timeStamp);
@@ -29,31 +26,33 @@ $(document).ready(function () {
   };
 
   var userFeed = function (selectedUser) {
+    $update.text('Back');
     $form.hide();
-    $back.show();
-    $update.hide();
     $feed.empty();
 
     updateFeed(selectedUser);
   }
 
-  $twiddlers.on('click', '#shawndrost', function () { userFeed('shawndrost') });
-  $twiddlers.on('click', '#sharksforcheap', function () { userFeed('sharksforcheap') });
-  $twiddlers.on('click', '#mracus', function () { userFeed('mracus') });
-  $twiddlers.on('click', '#douglascalhoun', function () { userFeed('douglascalhoun') });
+  $('.feed-container, .friends-list').on('click', '.shawndrost', function () { userFeed('shawndrost') });
+  $('.feed-container, .friends-list').on('click', '.sharksforcheap', function () { userFeed('sharksforcheap') });
+  $('.feed-container, .friends-list').on('click', '.mracus', function () { userFeed('mracus') });
+  $('.feed-container, .friends-list').on('click', '.douglascalhoun', function () { userFeed('douglascalhoun') });
 
   $update.on('click', function (event) {
     $feed.empty();
     updateFeed();
   });
 
-  $('#home, #back').on('click', function (event) {
+  $('#home, #update-feed').on('click', function (event) {
     if ($form.is(':visible')) {
       return;
     }
 
+    if ($update.text() === 'Back') {
+      $update.text('Update Feed');
+    }
+
     $form.show();
-    $back.hide();
     $update.show();
     $feed.empty();
     updateFeed();
