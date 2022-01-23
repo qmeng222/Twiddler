c1539ceefd9a01e81cae3535739a5732e3028d11
Amina <amina.thiam.pro@gmail.com>
2022-01-23 19:32:05 +0100

    Tweet components added

--- app.js
+++ app.js
@@ -16,11 +16,35 @@ $(document).ready(function(){
   var renderFeed = function() {
     $($feed).html('');
     var index = streams.home.length - 1;
+
     while(index >= 0){
       var tweet = streams.home[index];
       var $tweet = $('<div class="tweet"></div>');
-      $tweet.text(`@${tweet.user}: ${tweet.message}`);
+      var $userPhoto = $('<img class="profile-photo">');
+      var $username = $('<span class="username"></span>');
+      var $message = $('<p class="message"></p>');
+      var $timestamp = $('<span class="timestamp"></span>');
+      var $commentIcon = $('<img class="icon comment" src="assets/icons/placeholder.png">');
+      var $retweetIcon = $('<img class="icon retweet" src="assets/icons/placeholder.png">');
+      var $likeIcon = $('<img class="icon like" src="assets/icons/placeholder.png">');
+      var $shareIcon = $('<img class="icon share" src="assets/icons/placeholder.png">');
+
+      $($userPhoto).attr('src', `${tweet.profilePhotoURL}`);
+      $username.text(`@${tweet.user}`);
+      $username.text(`@${tweet.user}`);
+      $message.text(`${tweet.message}`);
+      $timestamp.text(`${tweet.created_at}`);
+
       $tweet.appendTo($feed);
+      $userPhoto.appendTo($tweet);
+      $username.appendTo($tweet);
+      $message.appendTo($tweet);
+      $timestamp.appendTo($tweet);
+      $commentIcon.appendTo($tweet);
+      $retweetIcon.appendTo($tweet);
+      $likeIcon.appendTo($tweet);
+      $shareIcon.appendTo($tweet);
+
       index -= 1;
     }
   };
