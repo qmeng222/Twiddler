234a35a260f6a0893ac9e05cd4763532ba6eea98
Kieran <kieran.yun@gmail.com>
2022-01-23 14:33:03 -0800

    Finished up to 'user feed section' currently unable to click on username to make it change the feed.

---
+++ app.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+
+  // Select already existing elements
+  var $app = $('#app');
+    $app.html('');
+
+    // Create new HTML elements
+  var $title = $('<h1>Twiddler</h1>')
+  var $updateFeedBtn = $('<button id="update-feed" type="button">Update Feed</button>')
+  var $feed = $('<section id="feed">Feed</section>');
+    var formatTweet = function (tweet) {
+      var $tweet = $('<div class="tweet"></div>');
+      var $img = $(`<img class="profile-photo" src=${streams.users[tweet.user][0].profilePhotoURL} alt=${tweet.user}></img>`);
+      var $user = $(`<div class="username">@${tweet.user}</div>`);
+      var $timestamp = $(`<div class="timestamp">${$.timeago(tweet.created_at)}</div>`);
+      var $tweetMessage = $(`<p class="message">${tweet.message}</p>`)
+      var $iconsSection = $('<div class="icon-container"></div>');
+      var $commentIcon = $('<i class="fas fa-comments icon comment"></i>');
+      var $retweetIcon = $('<i class="fas fa-retweet icon retweet"></i>');
+      var $likeIcon = $('<i class="fas fa-thumbs-up icon like"></i>');
+      var $shareIcon = $('<i class="fas fa-share-square icon share"></i>');
+      $iconsSection.append([$commentIcon, $retweetIcon, $likeIcon, $shareIcon])
+      $tweet.append([$img, $user, $timestamp, $tweetMessage, $iconsSection])
+      return $tweet
+    }
+
+    var renderFeed = function (user) {
+      $feed.html('');
+      if (user === undefined) {
+        var index = streams.home.length - 1;
+        while(index >= 0){
+          var tweet = streams.home[index];
+          var $tweet = formatTweet(tweet);
+          $tweet.appendTo($feed);
+          index -= 1;
+          }
+      } else {
+        var index = streams.users[user].length - 1;
+        while(index >= 0){
+          var tweet = streams.users[user][index];
+          var $tweet = formatTweet(tweet);
+          $tweet.appendTo($feed);
+          index -= 1;
+          }
+      }
+    }
+
+  renderFeed();
+  $users=$('.username')
+
+    // Create event handler functions
+
+  var handleUpdateFeedClick = function (event) {
+    console.log(event);
+    renderFeed();
+    if (event.target.innerText === "back") {
+      $updateFeedBtn.text("Update Feed");
+    };
+  }
+
+  var handleTitleClick = function (event) {
+    console.log(event);
+    alert('the title of the page is: ' + event.target.innerText);
+  };
+
+  var handleUsernameClick = function(event) {
+      alert('clicked user');
+      renderFeed(event.target.innerText.split('@')[1]);
+      $updateFeedBtn.text('back');
+  }
+    // Set event listeners (providing appropriate handlers as input)
+  $updateFeedBtn.on("click", handleUpdateFeedClick);
+
+  $title.on("click", handleTitleClick);
+
+  $users.on("click", handleUsernameClick);
+
+    // Append new HTML elements to the DOM
+  $title.appendTo($app);
+  $updateFeedBtn.appendTo($app);
+  $feed.appendTo($app);
+  $users.appendTo($app);
+
+});
