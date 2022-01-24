75113bf60f4689316d3bba454985e24d8d2ef7b6
Julian Tam <juliant653@gmail.com>
2022-01-23 18:51:59 -0800

    Implemented CSS and Styling and Friends List

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 $(document).ready(function(){
+  // Select already exisiting elements
   var $app = $('#app');
   $app.html('');
 
-  // Select already exisiting elements
-
   // Create new HTML elements
+  var $header = $('<header></header>')
   var $title = $('<h1>Twiddler</h1>');
-  var $button = $('<button id=update-feed>Update Feed</button>');
-  var $feed = $('<div id=feed></div>');
+  var $updatebutton = $('<button id="update-feed">Update Feed</button>');
+  var $feed = $('<div id="feed"></div>');
+  var $sidebar = $('<div class="sidebar"></div>');
+  var $friendslist = $('<ul id="friends-list"></ul>');
+  var $friendstitle = $('<h3>Friends List</h3>');
 
   // Create event handler function
-  var renderFeed = function(array, user) {
-    var index = streams.home.length - 1;
+  var renderFeed = function(user) {
+    var index;
+    var stream;
+    if (user === undefined) {
+      index = streams.home.length - 1;
+      stream = streams.home;
+    } else {
+      index = streams.users[user].length - 1;
+      stream = streams.users[user];
+    }
     while(index >= 0) {
-      var tweet = streams.home[index];
+      var tweet = stream[index];
       var $tweet = $('<div class="tweet"></div>');
       var $photo = $('<img class="profile-photo">');
       var $user = $('<div class="username"></div>');
@@ -27,41 +38,57 @@ $(document).ready(function(){
 
       $photo.attr('src', 'assets/img/' + tweet.user + '.png')
       $user.text('@' + tweet.user);
+      $user.on('click', function(event) {
+        handleUsernameClick(event.target.innerText.slice(1));
+      })
       $message.text(tweet.message);
       $time.text(jQuery.timeago(tweet.created_at));
 
-      // $('<i class="far fa-comment"></i>').appendTo($comment);
-      // $('<i class="fas fa-retweet"></i>').appendTo($retweet);
-      // $('<i class="far fa-heart"></i>').appendTo($like);
-      // $('<i class="fas fa-share"></i>').appendTo($share);
-
-      $comment.appendTo($icon);
-      $retweet.appendTo($icon);
-      $like.appendTo($icon);
-      $share.appendTo($icon);
-
-      $photo.appendTo($tweet);
-      $user.appendTo($tweet);
-      $message.appendTo($tweet);
-      $time.appendTo($tweet);
-      $icon.appendTo($tweet);
+      $icon.append($comment, $retweet, $like, $share);
+      $tweet.append($photo, $user, $message, $time, $icon);
 
       $tweet.appendTo($feed);
       index -= 1;
     }
+  };
+
+  var handleUsernameClick = function(username) {
+    $('div.tweet').remove();
+    $updatebutton.text('Back');
+    renderFeed(username);
   }
 
-  renderFeed(streams.home);
+  var renderFriendsList = function() {
+    for (var user in streams.users) {
+      var $friend = $('<li class="friend"></li>');
+      $friend.text('@' + user);
+      $friend.on('click', function(event) {
+        handleUsernameClick(event.target.innerText.slice(1));
+      })
+      $friend.appendTo($friendslist);
+    }
+  };
 
   // Set event listeners
-  $button.on('click', function() {
+  $updatebutton.on('click', function() {
     $('div.tweet').remove();
-    renderFeed(streams.home);
+    $updatebutton.text('Update Feed');
+    renderFeed();
   });
 
+  // Call functions
+  renderFeed();
+  renderFriendsList();
+
   // Append new HTML elements to the DOM
-  $title.appendTo($app);
-  $button.appendTo($app);
+  $title.appendTo($header);
+  $updatebutton.appendTo($sidebar);
+  $friendstitle.appendTo($sidebar);
+  $friendslist.appendTo($sidebar);
+  $sidebar.appendTo($app);
+  $header.appendTo($app);
   $feed.appendTo($app);
+
+  window.isItBeautifulYet = true;
 
 });
