caa02e1bdafe5151f2d8e4521807037492338824
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-01-24 20:04:37 -0500

    I think this is actually almost done

--- app.js
+++ app.js
@@ -7,62 +7,61 @@ $(document).ready(function(){
 
   var $title = $('<h1 class="title">Twiddler</h1>');
     $title.appendTo($app);
-  var $refreshFeed = $('<div id="update-feed">Update Feed</div>');
+  var $refreshFeed = $('<div id="update-feed"></div>');
+    $refreshFeed.text('Update Feed');
     $refreshFeed.appendTo($app);
   var $feed = $('<div id="feed"></div>');
     $feed.appendTo($app);
 
-  var userArray = [];
-
   var tweetWriter = function() {
+    $( "#feed" ).empty();
+    for (var x = streamArray.length - 1; x >= 0; x--) {
+      appender(streamArray[x]);
+      $refreshFeed.text('Update Feed')
+      }
+    }
+
+  var appender = function(obj) {
+    // Profile Pic
+    console.log(obj);
+    var picture = 'assets/img/' + obj.user + '.png';
+    var $profilePic = $('<img class="profile-photo"></img>');
+      $profilePic.attr("src", picture);
+    // Icons
+    var $comment = $('<i class="icon comment fas fa-robot"></i>');
+    var $retweet = $('<i class="icon retweet fas fa-satellite-dish"></i>');
+    var $like = $('<i class="icon like fas fa-hand-spock"></i>');
+    var $share = $('<i class="icon share fas fa-globe"></i>');
+    // Tweet Icons
+    var $username = $('<div class="username"></div>');
+    var $message = $('<div class="message"></div>');
+    var $timestamp = $('<div class="timestamp"></div>');
+    var $tweet = $('<div class="tweet"></div>');
+
+    var user = $username.text('@' + obj.user);
+    var message = $message.text(obj.message);
+    var timeAgo = jQuery.timeago(obj.created_at);
+    var timestamp = $timestamp.text(timeAgo);
+
+
+    $($tweet).append($username, $message, $timestamp, $profilePic, $comment, $retweet, $like, $share, $profilePic);
+      $tweet.appendTo($feed);
+
+    $('.username').on("click", function() {
       $( "#feed" ).empty();
-
-      for (var x = streamArray.length - 1; x >= 0; x--) {
-          var tweetData = streamArray[x];
-
-          // Profile Pic
-          var picture = tweetData.user + '.png';
-          var $profilePic = $('<img class="profile-photo"></img>');
-          // $profilePic.attr("src", picture);
-          // Icons
-          var $comment = $('<i class="icon comment fas fa-robot"></i>');
-          var $retweet = $('<i class="icon retweet fas fa-satellite-dish"></i>');
-          var $like = $('<i class="icon like fas fa-hand-spock"></i>');
-          var $share = $('<i class="icon share fas fa-globe"></i>');
-          // Tweet Icons
-          var $username = $('<div class="username"></div>');
-          var $message = $('<div class="message"></div>');
-          var $timestamp = $('<div class="timestamp"></div>');
-          var $tweet = $('<div class="tweet"></div>');
-
-          var user = $username.text('@' + tweetData.user);
-          var message = $message.text(tweetData.message);
-          var timeAgo = jQuery.timeago(tweetData.created_at);
-          var timestamp = $timestamp.text(timeAgo);
-
-          $($username).on("click", function() {
-            $( "#feed" ).empty();
-            for (var y = streamArray.length - 1; y >= 0; y--) { // variable values not updating inside the iteration
-              // I don't think anything else has to happen right here
-              if ('@' + streamArray[y].user === event.target.innerHTML) {  // click event not resetting
-              userArray.push(streamArray[y]);  // nothing else needs to happen here
-              }
-            }
-            for (var z = userArray.length - 1; z >= 0; z--) {
-              // values need to be updated here, passing the same message over and over
-                // nothing is being updated so only one unique tweet exists
-              appender(userArray[z]);
-            }
-          });
-
-          var appender = function(obj) {
-            $($tweet).append($username, $message, $timestamp, $profilePic, $comment, $retweet, $like, $share, $profilePic);
-            $tweet.appendTo($feed);
-            document.get.innerHTML = '';
-          }
-          appender(tweetData);
+      $refreshFeed.text('Back');
+      console.log(event);
+      for (var y = streamArray.length - 1; y >= 0; y--) {
+        // I don't think anything else has to happen right here
+        if ('@' + streamArray[y].user === event.target.outerText) {
+        appender(streamArray[y]);  // nothing else needs to happen here
         }
       }
+
+
+    });
+  }
+
   $(window).load(tweetWriter);
   $refreshFeed.click(tweetWriter);
 });
