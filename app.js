5ec66ba05e9d32f777fb8d66dfea2012c90659a5
Michelle Lane <michellecolemanlane@gmail.com>
2022-01-25 22:22:55 -0500

    Commit 2 started feed
    borders around my elements to use as a style guide, have tweet pieces established, used timeago,

--- app.js
+++ app.js
@@ -3,39 +3,54 @@
         var $app = $('#app');
         $app.html('');
 
+// creating elements
+var $title = $('<div class="header"><h1>Twiddler</h1></div>');
+
+
+var $like = $('<li class="icons"></li>');
+var $retweet = $('<li class="icons"></li>');
+var $share = $('<li class="icons icons_"></li>');
+var $comment = $('<li class="icons"></li>');
+
+
+var $button = $('<button id="update-feed">Update Feed</button>');
+var $feed = $('<div id="feed"></div>');
+
+//appendItems
+$title.appendTo($app);
+$button.appendTo($app);
+$feed.appendTo($app);
+// event handlers
+
+
+// create feed
 
 
 
 
+//organize feed
 
 
-//title on page
-        var $title = $('<h1>Twiddler</h1>');
-        $title.appendTo($app);
-				$title.on("click", function () {
-			console.log(event);
-			alert('The title of this page is ' + event.target.innerText)
-			})
-
-// creating a button
-
-	var $button = $('<button class="feed__button>Update Feed</button')
-			$button.appendTo($app);
-			$button.on('click', function () {
-			event.preventDefault;
-
-		})
-
-
-
-
+//Renders feed
         var index = streams.home.length - 1;
         while(index >= 0){
           var tweet = streams.home[index];
-          var $tweet = $('<div class="tweet"></div>');
-          $tweet.text('@' + tweet.user + ': ' + tweet.message);
-          $tweet.appendTo($app);
+					var $tweet = $('<div class="tweet"></div>');
+					var $profilePhoto = $('<img class="profile__photo" src="assets/img/' + tweet.user + '.png">');
+					var $message = $('<p class="tweet__message"></p>');
+					var $username = $('<span class="username"></span>');
+				var $timestamp = $('<span class="timestamp"></span>');
+					$timestamp.text($.timeago(tweet.created_at))
+					$timestamp.appendTo($tweet);
+					$username.text(tweet.user);
+					$username.prependTo($tweet);
+          $message.text( tweet.message);
+					$message.appendTo($tweet);
+					$profilePhoto.prependTo($tweet);
+          $tweet.appendTo($feed);
           index -= 1;
         }
 
       });
+
+
