1f386ab3fd0bb8538ed969fac462f846ee3622a7
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-01-25 19:01:33 -0800

    Refactored code. Made it more DRY!

--- app.js
+++ app.js
@@ -18,23 +18,9 @@ $(document).ready(function(){
 
     // Friends List
   var $friendsList = $('<ul id="friends-list">Friends List</ul>');
-  var $friend = $('<li class="friend">Friend</li>');
-  var $friendPhoto = $('<img class="profile-photo">');
-  var $friendUsername = $('<span class="friend-username"></span>');
-  var $friendName = $('<span class="friend-name"></span>');
 
     // Twiddler Feed
   var $feed = $('<div id="feed"></div');
-  var $tweet = $('<div class="tweet"></div>');
-  var $profilePhoto  = $('<img class="profile-photo">');
-  var $username = $('<span class="username"></span>');
-  var $message = $('<p class="message"></p>');
-  var $timestamp = $('<span class="timestamp"></span>');
-  var $icons = $('<div class="icon"></div>')
-  var $comment = $('<i class="icon comment far fa-comment-alt"></i>');
-  var $retweet = $('<i class="icon retweet fas fa-redo"></i>');
-  var $like = $('<i class="icon like far fa-heart"></i>');
-  var $share = $('<i class="icon share far fa-share-square"></i>');
 
   // Create event handler functions
   var handleTitleClick = function(event) {
@@ -77,7 +63,6 @@ $(document).ready(function(){
   // Set event listeners (providing appropriate handlers as input)
   $title.on("click", handleTitleClick);
   $btn.on("click", handleButtonClick);
-  $username.on("click", handleUsernameClick);
   $newTwidButton.on("click", handleTwidButtonClick);
 
 
@@ -103,74 +88,46 @@ $(document).ready(function(){
 
   // Starting Page
   var renderFeed = function(user) {
-    if (user === undefined) {
-      $feed.empty();
-      var index = streams.home.length - 1;
-      while(index >= 0){
-        var tweet = streams.home[index];
-        $tweet = $('<div class="tweet"></div>');
-        $profilePhoto = $('<img class="profile-photo" src="assets/img/' + tweet.user + '.png">');
-        $username = $('<span class="username">@' + tweet.user + '</span>');
-        $message = $('<p class="message">' + tweet.message + '</p>');
-        $timestamp = $('<span class="timestamp">' + jQuery.timeago(tweet.created_at) + '</span>');
-        $icons = $('<div class="icon"></div>');
-        $comment = $('<i class="icon comment far fa-comment-alt"></i>');
-        $retweet = $('<i class="icon retweet fas fa-redo"></i>');
-        $like = $('<i class="icon like far fa-heart"></i>');
-        $share = $('<i class="icon share far fa-share-square"></i>');
-        $tweet.appendTo($feed);
-        $profilePhoto.appendTo($tweet);
-        $username.appendTo($tweet);
-        $message.appendTo($tweet);
-        $timestamp.appendTo($tweet);
-        $icons.appendTo($tweet);
-        $comment.appendTo($icons);
-        $retweet.appendTo($icons);
-        $like.appendTo($icons);
-        $share.appendTo($icons);
-        $username.on("click", handleUsernameClick);
-        index -= 1;
-      }
-    } else {
-      $feed.empty();
-      var index = streams.home.length - 1;
-      while(index >= 0){
-        var tweet = streams.home[index];
-        if (user === tweet.user) {
-          $tweet = $('<div class="tweet"></div>');
-          $profilePhoto = $('<img class="profile-photo" src="assets/img/' + tweet.user + '.png">');
-          $username = $('<span class="username">@' + tweet.user + '</span>');
-          $message = $('<p class="message">' + tweet.message + '</p>');
-          $timestamp = $('<span class="timestamp">' + jQuery.timeago(tweet.created_at) + '</span>');
-          $icons = $('<div class="icon"></div>');
-          $comment = $('<i class="icon comment far fa-comment-alt"></i>');
-          $retweet = $('<i class="icon retweet fas fa-redo"></i>');
-          $like = $('<i class="icon like far fa-heart"></i>');
-          $share = $('<i class="icon share far fa-share-square"></i>');
-          $tweet.appendTo($feed);
-          $profilePhoto.appendTo($tweet);
-          $username.appendTo($tweet);
-          $message.appendTo($tweet);
-          $timestamp.appendTo($tweet);
-          $icons.appendTo($tweet);
-          $comment.appendTo($icons);
-          $retweet.appendTo($icons);
-          $like.appendTo($icons);
-          $share.appendTo($icons);
-          index -= 1;
-        }
-        index -= 1;
-      }
+    var currentArray = streams.home;
+    if (user) {
+      currentArray = streams.users[user];
+    }
+    $feed.empty();
+    var index = currentArray.length - 1;
+    while(index >= 0){
+      var tweet = currentArray[index];
+      var $tweet = $('<div class="tweet"></div>');
+      var $profilePhoto = $('<img class="profile-photo" src="assets/img/' + tweet.user + '.png">');
+      var $username = $('<span class="username">@' + tweet.user + '</span>');
+      var $message = $('<p class="message">' + tweet.message + '</p>');
+      var $timestamp = $('<span class="timestamp">' + jQuery.timeago(tweet.created_at) + '</span>');
+      var $icons = $('<div class="icon"></div>');
+      var $comment = $('<i class="icon comment far fa-comment-alt"></i>');
+      var $retweet = $('<i class="icon retweet fas fa-redo"></i>');
+      var $like = $('<i class="icon like far fa-heart"></i>');
+      var $share = $('<i class="icon share far fa-share-square"></i>');
+      $tweet.appendTo($feed);
+      $profilePhoto.appendTo($tweet);
+      $username.appendTo($tweet);
+      $message.appendTo($tweet);
+      $timestamp.appendTo($tweet);
+      $icons.appendTo($tweet);
+      $comment.appendTo($icons);
+      $retweet.appendTo($icons);
+      $like.appendTo($icons);
+      $share.appendTo($icons);
+      $username.on("click", handleUsernameClick);
+      index -= 1;
     }
   }
 
   var createFriendsList = function() {
     for (var key in streams.users) {
       var user = key;
-      $friend = $('<li class="friend"></li>');
-      $friendPhoto = $('<img class="friend-photo" src="assets/img/' + user + '.png">');
-      $friendName = $('<span class="friend-name">' + user + '</span>');
-      $friendUsername = $('<span class="friend-username">@' + user + '</span>');
+      var $friend = $('<li class="friend"></li>');
+      var $friendPhoto = $('<img class="friend-photo" src="assets/img/' + user + '.png">');
+      var $friendName = $('<span class="friend-name">' + user + '</span>');
+      var $friendUsername = $('<span class="friend-username">@' + user + '</span>');
       $friend.appendTo($friendsList);
       $friendPhoto.appendTo($friend);
       $friendName.appendTo($friend);
