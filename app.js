3a80726300673b1a8dbd09c76a0dce4ebbb6997b
Ken Kurita <kenkurita@Kens-MacBook-Pro.local>
2022-01-25 23:04:48 -0800

    Pre-test

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
+
 $(document).ready(function(){
+
         // Select the div with the ID #app
         //var $app = $('#app');
         var $feed = $("#feed");
-      /*
-        $app.html('');
-        // Create an h1 element with the text "Twiddler"
-        var $title = $('<h1>Twiddler</h1>');
-
-        // Append the h1 element to the DOM, nested inside of the #app div
-        $title.appendTo($app);
-
-        // Set a click event listener on the h1 element
-        $title.on("click", function(event) {
-          console.log(event);
-          alert('the title of this page is ' + event.target.innerText)
-        });
-
-        // Update Feed Button
-        var $update = $("<button type='button' id='updated-feed'>Update Feed</button>");
-        $update.appendTo($app)
-        $update.click(function() {
-          $tweet.text('@' + tweet.user + ': ' + tweet.message);
-        });
-      */
 
         $("#heading").on("click", function() {
           console.log(event);
@@ -37,9 +18,30 @@ $(document).ready(function(){
           //console.log(tweet)
           var $tweet = $('<div class="tweet"></div>');
           var $message=$('<div class="message"></div>');
-          $tweet.text('@' + tweet.user + ': ' );
+          var $username = $('<div class="username"></div>');
+          var $img = $('<img class="profile-photo">');
+          var $timeStamp = $('<div class="timestamp"></div>');
+          var $comment = $('<i class="fas fa-comments comment"></i>');
+          var $retweet = $('<i class="fas fa-retweet retweet"></i>');
+          var $like = $('<i class="fas fa-thumbs-up like"></i>');
+          var $share = $('<i class="fas fa-share-square share"></i>');
+          //////////////////////////
+
+
+
+          $username.text('@' + tweet.user + ': ' );
           $message.text(tweet.message);
           $tweet.append($message);
+          $tweet.append($username);
+          $tweet.append($img);
+          $timeStamp.append(jQuery.timeago(tweet.created_at));
+          $tweet.append($timeStamp);
+          $tweet.append($comment);
+          $tweet.append($retweet);
+          $tweet.append($like);
+          $tweet.append($share);
+
+
           $tweet.appendTo($feed);
           index -= 1;
         }
@@ -61,30 +63,100 @@ $(document).ready(function(){
           var $username = $('<div class="username"></div>');
           var $img = $('<img class="profile-photo">');
           var $timeStamp = $('<div class="timestamp"></div>');
-          var $comment = $('<div class="comment"></div>');
-          var $retweet = $('<div class="retweet"></div>');
-          var $like = $('<div class="like"></div>');
-          var $share = $('<div class="share"></div>');
-          $message.append(tweet.message);
+          var $comment = $('<i class="fas fa-comments comment"></i>');
+          var $retweet = $('<i class="fas fa-retweet retweet"></i>');
+          var $like = $('<i class="fas fa-thumbs-up like"></i>');
+          var $share = $('<i class="fas fa-share-square share"></i>');
+          //////////////////////////////////
+
+          $username.text('@' + tweet.user + ': ' );
+          $message.text(tweet.message);
           $tweet.append($message);
           $tweet.append($username);
-          $tweet.appendTo($feed);
-          // adding username to username div
-          $username.append("@" + tweet.user);
-          // adding image to tweet
           $tweet.append($img);
-          // adding time stamp data to timestamp
-          console.log(tweet.created_at)
-          $timeStamp.append(tweet.created_at);
-          // adding timestamp to tweet
+          $timeStamp.append(jQuery.timeago(tweet.created_at));
           $tweet.append($timeStamp);
+          $tweet.append($comment);
+          $tweet.append($retweet);
+          $tweet.append($like);
+          $tweet.append($share);
 
+
+          $tweet.appendTo($feed);
           index -= 1;
 
         }
 
-        });
       });
+
+
+      // on user click
+      $("#feed").on("click", "div .username", function(event) {
+        // filter tweets to show only the users'
+        var user = ($(this).text())
+        //$(".username").not(this).hide();
+
+
+
+        const username =user.substring(1, user.length - 2)
+        console.log(username)
+        console.log(user, streams.users[username])
+
+        $feed.empty();
+        var index = streams.users[username].length - 1;
+        //console.log(streams)
+        while(index >= 0){
+
+          var tweet = streams.users[username][index];
+          //console.log(tweet)
+          // feature short-hand names
+          var $tweet = $('<div class="tweet"></div>');
+          var $message=$('<div class="message"></div>');
+          var $username = $('<div class="username"></div>');
+          var $img = $('<img class="profile-photo">');
+          var $timeStamp = $('<div class="timestamp"></div>');
+          var $comment = $('<i class="fas fa-comments comment"></i>');
+          var $retweet = $('<i class="fas fa-retweet retweet"></i>');
+          var $like = $('<i class="fas fa-thumbs-up like"></i>');
+          var $share = $('<i class="fas fa-share-square share"></i>');
+          //////////////////////////////////
+
+          $username.text('@' + tweet.user + ': ' );
+          $message.text(tweet.message);
+          $tweet.append($message);
+          $tweet.append($username);
+          $tweet.append($img);
+          $timeStamp.append(jQuery.timeago(tweet.created_at));
+          $tweet.append($timeStamp);
+          $tweet.append($comment);
+          $tweet.append($retweet);
+          $tweet.append($like);
+          $tweet.append($share);
+
+
+          $tweet.appendTo($feed);
+          index -= 1;
+
+        }
+
+
+
+
+
+        // change button to ready "Back"
+        $("#update-feed").text("Back");
+
+        $("#update-feed").on("click", function() {
+          $("#update-feed").text("Update Feed");
+        })
+
+
+      })
+
+
+
+
+    });
 
 
 
