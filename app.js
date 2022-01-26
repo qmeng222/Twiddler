4f1e038e31beefc701b93bed6b7a094fbbd39e92
Stephanie Yeong <sfy2368@gmail.com>
2022-01-25 23:50:22 -0500

    passed testsuites

--- app.js
+++ app.js
@@ -7,54 +7,44 @@ $(document).ready(function(){
   var $title = $('<h1>Twiddler</h1>');
   $title.appendTo($app);
   $title.on("click", function(event) {
-  console.log(event);
-  alert('The title of this page is: ' + event.target.innerText);
+    console.log(event);
+    alert('The title of this page is: ' + event.target.innerText);
   });
 
 //Update Button Made
-  var $update = $('<button>Update Feed</button>');$update.appendTo($app);
+  var $update = $('<button id="update-feed">Update Feed</button>');$update.appendTo($app);
 
-//Home Button Made
-  var $home = $('<button>Home</button>');$home.appendTo($app);
 
 //Create Feed Element
   var $feed = $('<div id="feed"></div>');
   $feed.appendTo($app);
 
-//Create Span To Use For User
-  var $span = $('span');
+//Hover
+  var inMouse = function() {
+    $(this).css("color", "#262220");
+  };
+
+  var outMouse = function() {
+    $(this).css("color", "#A15C38");
+  };
+
 
 //Gets New Tweets
   var newTweets = function() {
     $feed.html('');
     var index = streams.home.length - 1;
     while (index >= 0){
-
       var tweet = streams.home[index];
       var $tweet = $('<div class="tweet"></div>');
 
-      var $photo = $('<div class="profile-photo"></div>');
-        if (tweet.user === 'douglascalhoun'){
-          $('<img src="assets/img/douglascalhoun.png" alt="" width="50px"/>').appendTo($photo);
-        }
-        if (tweet.user === 'mracus'){
-          $('<img src="assets/img/mracus.png" alt="" width="50px"/>').appendTo($photo);
-        }
-        if (tweet.user === 'sharksforcheap'){
-          $('<img src="assets/img/sharksforcheap.png" alt="" width="50px"/>').appendTo($photo);
-        }
-        if (tweet.user === 'shawndrost'){
-          $('<img src="assets/img/shawndrost.png" alt="" width="50px"/>').appendTo($photo);
-          $photo.appendTo($tweet);
-        }
-      $photo.appendTo($tweet);
+      $('<img class="profile-photo" img src="assets/img/' + tweet.user + '.png" alt="" width="50px"/>').appendTo($tweet);
 
       var $message = $('<div class="message"></div>');
       $message.html(tweet.message);
       $message.appendTo($tweet);
 
       var $username = $('<div class="username"></div>');
-      $username.html('@<span class="' + tweet.user + '">' + tweet.user + '</span>');
+      $username.html('@' + tweet.user);
       $username.appendTo($tweet);
 
       var $timestamp = $('<div class="timestamp"></div>');
@@ -62,10 +52,10 @@ $(document).ready(function(){
       $timestamp.appendTo($tweet);
 
       var $icon = $('<div class="icon"></div>');
-        $('<img class="like" img src="assets/icons/Like.png" alt="" width="33px"/>').appendTo($icon);
-        $('<img class="comment" img src="assets/icons/Comment.png" alt="" width="29px"/>').appendTo($icon);
-        $('<img class="retweet" img src="assets/icons/Retweet.png" alt="" width="30.5px"/>').appendTo($icon);
-        $('<img class="share" img src="assets/icons/Share.png" alt="" width="32px"/>').appendTo($icon);
+        $('<i class="like far fa-heart fa-lg"></i>').on({"mouseover": inMouse, "mouseout": outMouse}).appendTo($icon);
+        $('<i class="comment far fa-comments fa-lg"></i>').on({"mouseover": inMouse, "mouseout": outMouse}).appendTo($icon);
+        $('<i class="retweet fas fa-retweet fa-lg"></i>').on({"mouseover": inMouse, "mouseout": outMouse}).appendTo($icon);
+        $('<i class="share fas fa-share fa-lg"></i>').on({"mouseover": inMouse, "mouseout": outMouse}).appendTo($icon);
       $icon.appendTo($tweet);
       $tweet.appendTo($feed);
 
@@ -78,63 +68,35 @@ $(document).ready(function(){
 //Puts Intital Tweets On Page
   newTweets();
 
-//Show User Tweets
-  // var userTweets = function(person) {
-  //   $feed.html('');
-
-   //   newTweets(tweet);
-  // };
 
 //Calls On Update
   $update.on("click", function(newUpdates) {
     newUpdates.preventDefault();
     $update.text('Update Feed');
     newTweets();
-    });
+  });
 
-//Go Home
-  $home.on("click", function(event) {
-    console.log(event);
-    alert('We Home: ' + event.target.innerText);
-    });
-
-//Back Button
-  //var $back = $('<button>Go Back</button>');$back.appendTo($app);
 
 
 //Click User
-$feed.on('click', 'span', function(event) {
-  $feed.html('');
-  $update.text('Back');
-  var username = this.className;
-    var index = streams.users[username].length - 1;
-    while (index >= 0){
+  $feed.on('click', '.username', function(event) {
+    $feed.html('');
+    $update.text('Back');
+    var username = event.target.innerText.slice(1);
 
+    var index = streams.users[username].length -1;
+    while (index >= 0){
       var tweet = streams.users[username][index];
       var $tweet = $('<div class="tweet"></div>');
 
-      var $photo = $('<div class="profile-photo"></div>');
-        if (tweet.user === 'douglascalhoun'){
-          $('<img src="assets/img/douglascalhoun.png" alt="" width="50px"/>').appendTo($photo);
-        }
-        if (tweet.user === 'mracus'){
-          $('<img src="assets/img/mracus.png" alt="" width="50px"/>').appendTo($photo);
-        }
-        if (tweet.user === 'sharksforcheap'){
-          $('<img src="assets/img/sharksforcheap.png" alt="" width="50px"/>').appendTo($photo);
-        }
-        if (tweet.user === 'shawndrost'){
-          $('<img src="assets/img/shawndrost.png" alt="" width="50px"/>').appendTo($photo);
-          $photo.appendTo($tweet);
-        }
-      $photo.appendTo($tweet);
+      $('<img class="profile-photo" img src="assets/img/' + tweet.user + '.png" alt="" width="50px"/>').appendTo($tweet);
 
       var $message = $('<div class="message"></div>');
       $message.html(tweet.message);
       $message.appendTo($tweet);
 
       var $username = $('<div class="username"></div>');
-      $username.html('@<span class="' + tweet.user + '">' + tweet.user + '</span>');
+      $username.html('@' + tweet.user);
       $username.appendTo($tweet);
 
       var $timestamp = $('<div class="timestamp"></div>');
@@ -142,18 +104,17 @@ $feed.on('click', 'span', function(event) {
       $timestamp.appendTo($tweet);
 
       var $icon = $('<div class="icon"></div>');
-        $('<img class="like" img src="assets/icons/Like.png" alt="" width="33px"/>').appendTo($icon);
-        $('<img class="comment" img src="assets/icons/Comment.png" alt="" width="29px"/>').appendTo($icon);
-        $('<img class="retweet" img src="assets/icons/Retweet.png" alt="" width="30.5px"/>').appendTo($icon);
-        $('<img class="share" img src="assets/icons/Share.png" alt="" width="32px"/>').appendTo($icon);
+        $('<i class="like far fa-heart fa-lg"></i>').on({"mouseover": inMouse, "mouseout": outMouse}).appendTo($icon);
+        $('<i class="comment far fa-comments fa-lg"></i>').on({"mouseover": inMouse, "mouseout": outMouse}).appendTo($icon);
+        $('<i class="retweet fas fa-retweet fa-lg"></i>').on({"mouseover": inMouse, "mouseout": outMouse}).appendTo($icon);
+        $('<i class="share fas fa-share fa-lg"></i>').on({"mouseover": inMouse, "mouseout": outMouse}).appendTo($icon);
       $icon.appendTo($tweet);
       $tweet.appendTo($feed);
 
       index -= 1;
     }
- });
 
-
+  });
 
 
 });
