e6f813853deaf6066881755eaebe918132e7815c
Austin Chapin <austinchapin04@gmail.com>
2022-01-25 22:43:33 -0800

    Passes tests up to Styling section of tests

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-
   jQuery(document).ready(function() {
     jQuery("time.timeago").timeago();
   });
@@ -11,38 +10,33 @@ $(document).ready(function(){
 
 
 
-  // CREATE NEW HTML ELEMENTS
+  // CREATE NEW HTML ELEMENTS AND GLOBAL VARIABLES
   var $title = $('<h1 class="primary-header">Twiddler</>');
   var $updateFeedBtn = $('<button type=button class="btn" id=update-feed>Update Feed</button>');
-  var $feed = $('<div id="feed"></div>')
-
-  var stream = streams.home;
-  console.log(stream);
+  var $feed = $('<div id="feed"></div>');
 
 
-
-
-  // CREATE EVENT HANDLER FUNCTIONS
-  var renderFeed = function() {
+  var renderFeed = function(user) {
     $feed.html('');
-    addTweetsToFeed();
-  }
-
-
-  var addTweetsToFeed = function () {
-    var index = streams.home.length - 1;
+    if (user) {
+      var userTweets = streams.users[user];
+      var index = userTweets.length - 1;
+    } else {
+      index = streams.home.length - 1;
+      userTweets = streams.home;
+    }
     while(index >= 0){
-      var tweet = streams.home[index];
+      var tweet = userTweets[index];
+      var tweetAuthor = user ? user : tweet.user;
       var $tweet = $('<div class="tweet"></div>');
-      var $username = $('<div class="username">' + '@' + tweet.user + '</div>');
-      var $pp = $('<img class="profile-photo" src="assets/img/' + tweet.user + '.png">');
+      var $username = $('<div class="username">' + '@' + tweetAuthor + '</div>');
+      var $pp = $('<img class="profile-photo" src="assets/img/' + tweetAuthor + '.png">');
       var $tweetMsg = $('<div class="message">' + tweet.message + '</div>');
       var $timeStamp = $('<div class="timestamp">' + jQuery.timeago(tweet.created_at) + '</div>');
       var $comment = $('<i class="far fa-comment comment"></i>');
       var $retweet = $('<i class="fas fa-retweet retweet"></i>');
       var $like = $('<i class="fas fa-thumbs-up like"></i>');
       var $share = $('<i class="fas fa-share share"></i>');
-
       $pp.appendTo($tweet)
       $username.appendTo($tweet)
       $tweetMsg.appendTo($tweet)
@@ -55,12 +49,17 @@ $(document).ready(function(){
       $tweet.appendTo($feed);
       index -= 1;
     };
-  }
-  addTweetsToFeed();
 
-  // jQuery.timeago(new Date())
-  // jQuery.timeago(`${tweet.created_at.toString()}`)
-  // `${tweet.created_at.toString()}`
+  }
+  renderFeed();
+
+
+  // CREATE EVENT HANDLER FUNCTIONS
+
+  var handleUsernameClick = function() {
+    $(this).text($(this).text() === 'Back' ? $updateFeedBtn.text() : 'Back');
+  }
+
 
 
   // SET EVENT LISTENERS
@@ -68,8 +67,18 @@ $(document).ready(function(){
     renderFeed();
   });
 
+  $updateFeedBtn.on("click", handleUsernameClick)
+
+  $(document).on("click", ".username", function() {
+    renderFeed($(this).text().slice(1));
+    $updateFeedBtn.html('Back')
+  })
 
 
+   // Toggle over icon - placement?
+  //  $('.icon').mouseover(function() {
+  //   $('.icon').toggleClass(***CHANGE COLOR***);
+  // })
 
 
 
@@ -82,36 +91,3 @@ $(document).ready(function(){
 
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // var $pp;
-  // var $username;
-  // var $tweetMsg;
-  // var $timeStamp;
-  // var $comment;
-  // var $retweet;
-  // var $like;
-  // var $share;
