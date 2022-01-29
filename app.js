1bf9c493f5b140031fb6c1fdb9dc91434416b28e
Blake Crawford <blake.crawford@outlook.com>
2022-01-28 22:27:29 -0800

    added tweet UI components and refactored code for readability

--- app.js
+++ app.js
@@ -1,40 +1,69 @@
 $(document).ready(function(){
+  // Select already existing elements
   var $app = $('#app');
-  $app.html('');
 
+  // Create new HTML elements
   var $title = $('<h1>Twiddler</h1>');
-  $title.appendTo($app);
+  var $button = $('<button id="update-feed">Update Feed</button>');
+  var $feed = $('<div id="feed"></div>');
 
-  $title.on('click', function(event) {
+  var renderFeed = function(array) {
+    var index = streams.home.length - 1;
+    while(index >= 0){
+
+      // Create new HTML elements
+      var tweet = streams.home[index];
+      var $tweet = $('<div class="tweet"></div>');
+      var $profilePhoto = $('<img class="profile-photo" src=""></img');
+      var $userName = $('<div class="username"></div>');
+      var $message = $('<div class ="message"></div>');
+      var $timeStamp = $('<div class="timestamp"></div>');
+      var $comment = $('<img class="icon comment" src="assets/icons/placeholder.png"></img>');
+      var $retweet = $('<img class="icon retweet" src="assets/icons/placeholder.png"></img>');
+      var $like = $('<img class="icon like" src="assets/icons/placeholder.png"></img>');
+      var $share = $('<img class="icon share" src="assets/icons/placeholder.png"></img>');
+
+      // Select already existing elements
+      $userName.text('@' + tweet.user);
+      $message.text(tweet.message);
+      $timeStamp.text(new Date(tweet.created_at));
+
+      // Append new HTML elements to the DOM
+      $profilePhoto.appendTo($tweet);
+      $userName.appendTo($tweet);
+      $message.appendTo($tweet);
+      $timeStamp.appendTo($tweet);
+      $comment.appendTo($tweet);
+      $retweet.appendTo($tweet);
+      $like.appendTo($tweet);
+      $share.appendTo($tweet);
+      $tweet.appendTo($feed);
+
+      index -= 1;
+    }
+  }
+
+  renderFeed();
+
+  // Create event handler functions
+  var handleTitleClick = $title.on('click', function(event) {
     console.log(event);
     alert('The title of this page is: ' + event.target.innerText);
   });
 
-  var $button = $('<button id="update-feed">Update Feed</button>');
-  $button.appendTo($app);
-
-  var $feed = $('<div id="feed"></div>');
-  $feed.appendTo($app);
-
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div class="tweet"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($feed);
-    index -= 1;
-  }
-
   $button.on('click', function(event) {
     $('#feed').empty();
-    var index = streams.home.length - 1;
-    while(index >= 0){
-      var tweet = streams.home[index];
-      var $tweet = $('<div class="tweet"></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message);
-      $tweet.appendTo($feed);
-      index -= 1;
-    }
+    renderFeed();
   });
+
+  // Set event listeners (providing appropriate handlers as input)
+
+  // Append new HTML elements to the DOM
+  $app.html('');
+  $title.appendTo($app);
+  $button.appendTo($app);
+  $feed.appendTo($app);
+
 });
+
 
