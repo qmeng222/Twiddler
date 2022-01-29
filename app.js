30ec0b5303d25ebd786aa4688eb4c737492f354d
Michelle Lane <michellecolemanlane@gmail.com>
2022-01-28 19:50:08 -0500

    Commit 4
    Jquery is working.

--- app.js
+++ app.js
@@ -5,11 +5,6 @@
 
 // creating elements
 var $title = $('<div class="header"><h1>Twiddler</h1></div>');
-
-console.log(streams.home);
-
-
-
 var $button = $('<button id="update-feed">Update Feed</button>');
 var $feed = $('<div id="feed"></div>');
 
@@ -20,36 +15,70 @@ $feed.appendTo($app);
 
 
 //Renders feed
-var renderFeed = function (event) {
-        var index = streams.home.length - 1;
-        while(index >= 0){
-          var tweet = streams.home[index];
-var $tweet = $('<div class="tweet"></div>');
-					var $profilePhoto = $('<img class="profile__photo" src="assets/img/' + tweet.user + '.png">');
-					var $message = $('<p class="tweet__message"></p>');
+var renderFeed = function (userName) {
+        $feed.empty();
+// if there is a user - only show that users tweets from the streams.home
+// else show all tweets
+
+        if (userName) {
+      // array of tweets to render = username tweets only
+        var arrayofTweets = streams.users[userName];
+        } else {
+      //array of tweets to render is all tweets streams.home
+      var arrayofTweets = streams.home;
+        }
+
+
+ for (var i = arrayofTweets.length - 1; i >=0; i--) {
+
+// tweet will equal array of tweets [index];
+          var tweet = arrayofTweets[i];
+          var $tweet = $('<div class="tweet"></div>');
+					var $profilePhoto = $('<img class="profile-photo" src="assets/img/' + tweet.user + '.png">');
+					var $message = $('<p class="message"></p>');
 					var $username = $('<span class="username"></span>');
-				var $timestamp = $('<span class="timestamp"></span>');
-var $like = $('<img class="icons like" src="assets/img/like.gif">');
-var $retweet = $('<img class="icons retweet" src="assets/img/retweet-24.png">');
-var $share = $('<img class="icons share" src="assets/img/share-3-24.png">');
-var $comment = $('<img class="icons comment" src="assets/img/comments.gif">');
-					$username.text(tweet.user).prependTo($tweet);
-          $message.text( tweet.message).appendTo($tweet);
+				  var $timestamp = $('<span class="timestamp"></span>');
+          var $like = $('<i class=" like far fa-thumbs-up"></i>');
+          var $retweet = $('<i class="retweet fas fa-retweet"></i>');
+          var $share = $('<i class="share far fa-share-square"></i>');
+          var $comment = $('<i class="comment far fa-comment"></i>');
 					$profilePhoto.prependTo($tweet);
-						$timestamp.text($.timeago(tweet.created_at))
+          $username.text('@' + tweet.user).prependTo($tweet);
+					$timestamp.text($.timeago(tweet.created_at))
 					$timestamp.appendTo($tweet);
+          $message.text( tweet.message).appendTo($tweet);
 $like.appendTo($tweet);
 $comment.appendTo($tweet)
 $share.appendTo($tweet)
 $retweet.appendTo($tweet);
-  $tweet.appendTo($feed);
-          index -= 1;
-        }        }
+$tweet.appendTo($feed);
+
+var userNameClick = function () {
+var userName = $(this).text().slice(1);
+console.log(userName);
+$feed.empty();
+$button.text('Back');
+renderFeed(userName)
+
+
+}
+
+$username.on('click', userNameClick);
+
+}
+
+     }
+
+renderFeed();
 
 //event handlers
 
-$button.on('click', renderFeed);
+var updateButton = function () {
+renderFeed();
+$button.text('Update Feed');
+}
 
-      });
+$button.on("click", updateButton);
+});
 
 
