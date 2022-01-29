05b505ca7123d4f94ad0ab5f0d03db27e48d6286
Rob Scherer <robscherer6@gmail.com>
2022-01-29 13:51:31 -0500

    implementing update feed button

--- app.js
+++ app.js
@@ -11,19 +11,47 @@ $(document).ready(function(){
     alert('The title of this page: ' + event.target.innerText);
   })
 
-  //index of last tweet in collection
-  var index = streams.home.length - 1;
-  //iterate backwards
-  while(index >= 0){
-    //most recent tweet
-    var tweet = streams.home[index];
-    //UI element, create new div with class tweet
-    var $tweet = $('<div class="tweet"></div>');
-    //text of above tweet
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    //take div and put it on app
-    $tweet.appendTo($app);
-    index -= 1;
-  }
+  var $button = $('<button id=update-feed>Update Feed</button>');
+  $button.appendTo($app);
+
+  var $feed = $('<div id=feed></div>');
+  $feed.appendTo($app);
+
+  $button.on('click', function(event) {
+    console.log(event);
+    // Remove all previously existing Tweets from the Feed
+    $feed.html('');
+    //index of last tweet in collection
+    var index = streams.home.length - 1;
+    //iterate backwards
+    while(index >= 0){
+      //most recent tweet
+      var tweet = streams.home[index];
+      //UI element, create new div with class tweet
+      var $tweet = $('<div class="tweet"></div>');
+      //text of above tweet
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      //take div and put it on app
+      $tweet.appendTo($feed);
+      //decrement i (move to next most recent tweet)
+      index -= 1;
+    }
+  })
+
+    //index of last tweet in collection
+    var index = streams.home.length - 1;
+    //iterate backwards
+    while(index >= 0){
+      //most recent tweet
+      var tweet = streams.home[index];
+      //UI element, create new div with class tweet
+      var $tweet = $('<div class="tweet"></div>');
+      //text of above tweet
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      //take div and put it on app
+      $tweet.appendTo($app);
+      //decrement i (move to next most recent tweet)
+      index -= 1;
+    }
 
 });
