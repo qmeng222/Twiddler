58f8de14f4e1d27bd6c3455e4074dd228fa795f4
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-01-31 14:34:21 -0600

    pass all tests under Project

---
+++ app.js
@@ -0,0 +1,13 @@
+
+  var $app = $('#app');
+  $app.html('');
+
+  var index = streams.home.length - 1;
+  while(index >= 0){
+    var tweet = streams.home[index];
+    var $tweet = $('<div class="tweet"></div>');
+    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    $tweet.appendTo($app);
+    index -= 1;
+  }
+
