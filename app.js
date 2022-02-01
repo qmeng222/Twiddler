38e09ffcfb020d17541870b84e90538d242ed930
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-01-31 20:07:00 -0600

    rename helper function and add document.ready()

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
 
-var $app = $('#app');
-$app.html('');
+$(document).ready(function() {
+  var $app = $('#app');
+  $app.html('');
 
-var $title = $('<div id="title">Twiddler</div>');
-$title.appendTo($app);
+  var $title = $('<div id="title">Twiddler</div>');
+  $title.appendTo($app);
 
-var $updateFreed = $('<button id="update-feed">Update Feed</button>');
-$updateFreed.appendTo($app);
+  var $updateFreed = $('<button id="update-feed">Update Feed</button>');
+  $updateFreed.appendTo($app);
 
-var appendTweets = function(parent) {
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div class="tweet"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo(parent);
-    index -= 1;
+  var renderFeed = function(parent) {
+    var index = streams.home.length - 1;
+    while(index >= 0){
+      var tweet = streams.home[index];
+      var $tweet = $('<div class="tweet"></div>');
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      $tweet.appendTo(parent);
+      index -= 1;
+    }
   }
-}
-appendTweets($app);
+  renderFeed($app);
 
-var $feed = $('<div id="feed"></div>');
-$feed.appendTo($app);
+  var $feed = $('<div id="feed"></div>');
+  $feed.appendTo($app);
 
-var $tweets = $('.tweet');
-$tweets.appendTo($feed);
+  var $tweets = $('.tweet');
+  $tweets.appendTo($feed);
 
-// create an click event for update button
-$updateFreed.on("click", function(e) {
-  // Remove all previously existing Tweets from the Feed
-  $(".tweet").remove();
-  // For each Tweet object in the stream array (in reverse order)
-  appendTweets($feed);
+  // create an click event for update button
+  $updateFreed.on("click", function(e) {
+    // Remove all previously existing Tweets from the Feed
+    $(".tweet").remove();
+    // For each Tweet object in the stream array (in reverse order)
+    renderFeed($feed);
+  });
 });
 
 
