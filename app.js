735b186962f8f8f4f01f09371be8544375b78fda
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-01-31 18:50:57 -0600

    pass all test for Update Feed Button When Clicked

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 
-  var $app = $('#app');
-  $app.html('');
+var $app = $('#app');
+$app.html('');
 
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div class="tweet"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($app);
-    index -= 1;
-  }
+var $title = $('<div id="title">Twiddler</div>');
+$title.appendTo($app);
+
+var $updateFreed = $('<button id="update-feed">Update Feed</button>');
+$updateFreed.appendTo($app);
+
+var index = streams.home.length - 1;
+while(index >= 0){
+  var tweet = streams.home[index];
+  var $tweet = $('<div class="tweet"></div>');
+  $tweet.text('@' + tweet.user + ': ' + tweet.message);
+  $tweet.appendTo($app);
+  index -= 1;
+}
 
 var $feed = $('<div id="feed"></div>');
 $feed.appendTo($app);
@@ -17,5 +23,21 @@ $feed.appendTo($app);
 var $tweets = $('.tweet');
 $tweets.appendTo($feed);
 
-var $updateFreed = $('<button id="update-feed">Update</button>');
-$updateFreed.appendTo($app);
+// create an click event for update button
+$updateFreed.on("click", function(e) {
+
+  // Remove all previously existing Tweets from the Feed
+  $(".tweet").remove();
+  // For each Tweet object in the stream array (in reverse order)
+  var index = streams.home.length - 1;
+  while(index >= 0){
+    var tweet = streams.home[index];
+    var $tweet = $('<div class="tweet"></div>');
+    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    $tweet.appendTo($feed);
+    index -= 1;
+  }
+});
+
+
+
