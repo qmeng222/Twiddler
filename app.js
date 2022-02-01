7251e50ac217876207ffad6b75f4a76f30551a79
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-01-31 19:44:27 -0600

    refactor app.js

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ $title.appendTo($app);
 var $updateFreed = $('<button id="update-feed">Update Feed</button>');
 $updateFreed.appendTo($app);
 
-var index = streams.home.length - 1;
-while(index >= 0){
-  var tweet = streams.home[index];
-  var $tweet = $('<div class="tweet"></div>');
-  $tweet.text('@' + tweet.user + ': ' + tweet.message);
-  $tweet.appendTo($app);
-  index -= 1;
+var appendTweets = function(parent) {
+  var index = streams.home.length - 1;
+  while(index >= 0){
+    var tweet = streams.home[index];
+    var $tweet = $('<div class="tweet"></div>');
+    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    $tweet.appendTo(parent);
+    index -= 1;
+  }
 }
+appendTweets($app);
 
 var $feed = $('<div id="feed"></div>');
 $feed.appendTo($app);
@@ -25,19 +28,12 @@ $tweets.appendTo($feed);
 
 // create an click event for update button
 $updateFreed.on("click", function(e) {
-
   // Remove all previously existing Tweets from the Feed
   $(".tweet").remove();
   // For each Tweet object in the stream array (in reverse order)
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div class="tweet"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($feed);
-    index -= 1;
-  }
+  appendTweets($feed);
 });
+
 
 
 
