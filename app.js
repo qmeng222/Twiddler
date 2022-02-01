18f1f327652f1aa77f3171dd93f89838251a7254
Kate Anderson <kate.anderson250@gmail.com>
2022-02-01 12:50:05 -0800

    timeago and fonts in app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
+//Select existing HTML elements
+
   var $app = $('#app');
   $app.html('');
+
+//Create new HTML elements
 
   var $title = $('<h1>Twiddler</h1>');
   var $updateFeedButton = $("<button id = 'update-feed' type = 'button'>Update Feed</button>");
@@ -8,6 +12,39 @@ $(document).ready(function(){
 
 //Event Handlers
 
+  var renderTweet = function(tweet) {
+    //initiate tweet components
+    var $tweet = $('<div class="tweet"></div>');
+    var $profilePic = $('<img class = "profile-photo"></img>').attr("src", tweet.profilePhotoURL);
+    var $username = $('<div class = "username"></div>').text('@' + tweet.user);
+    var $message = $('<p class = "message"></p>').text(tweet.message);
+    var $timestamp = $('<div class = "timestamp"></div>').text(jQuery.timeago(tweet.created_at));
+    var $comment = $('<i class = "icon comment fas fa-comment"></i>');
+    var $retweet = $('<i class = "icon retweet fas fa-retweet"></i>');
+    var $like = $('<i class = "icon like fas fa-heart"></i>');
+    var $share = $('<i class = "icon share fas fa-share"></i>');
+
+    //now combine them together
+    $tweet.append($profilePic, $username, $message, $timestamp, $comment, $retweet, $like, $share);
+    return $tweet;
+  };
+
+  var renderFeed = function() {
+    $homeFeed.empty();
+    var index = streams.home.length - 1;
+    while(index >= 0){
+      var tweet = streams.home[index];
+      var $tweet = renderTweet(tweet);
+      $tweet.appendTo($homeFeed);
+      index -= 1;
+    }
+  };
+/*
+  var makeBlue = function() {
+    $this.css("color", "blue")
+  };
+*/
+/*
   var renderFeed = function() {
     $homeFeed.empty();
     var index = streams.home.length - 1;
@@ -19,12 +56,15 @@ $(document).ready(function(){
       index -= 1;
     }
   };
+ */
 
 
 ///Event listeners
 
+
   renderFeed(); //This one is special; it just runs every time the page is reloaded.
   $updateFeedButton.on("click", renderFeed);
+  $
 
 ///Append elements
   $title.appendTo($app);
