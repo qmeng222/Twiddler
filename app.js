da97d80f7bbda92824a0005e535df74bf7fd6511
Will Atwood <x1@galaxia.us>
2022-02-02 10:28:10 -0500

    Event listener delegation

--- app.js
+++ app.js
@@ -11,97 +11,39 @@ $(document).ready(function () {
 
   $("time.timeago").timeago();
 
-
-
   //Create event handler functions
   var handleTitleClick = function (event) {
     var titleType = event.target.localName === 'h1' ? 'title' : 'subtitle';
     alert(`The ${titleType} of this page is ${event.target.innerText}`)
   }
 
-  var renderFeed = function () {
+  var renderFeed = function (user = 'home') {
     $feed.html('');
-    var index = streams.home.length - 1;
+    var base = user === 'home' ? streams.home : streams.users[user];
+    var index = base.length - 1
+
+    console.log(index)
 
     while (index >= 0) {
-      var tweet = streams.home[index]; //no jQuery.. data from backend
-      var $tweet = $('<div class="tweet"></div>'); //use $ to create new div
-      var $message = $('<div class="message"></div>');
-      var $username = $('<div class="username"></div>');
-      var $timestamp = $('<div class="timestamp"></div><br>');
-
-      //profile photo
-      var imgsrc = streams.home[index].profilePhotoURL
-      $profilePhoto = $('<img>');
-      $profilePhoto.attr({ src: imgsrc, class: "profile-photo" });
-
-      //icons
-      faCommentIcon = "icon comment fas fa-thumbs-up fa-3x";
-      faRetweetIcon = "icon retweet fas fa-satellite-dish fa-3x";
-      faLikeIcon = "icon like fas fa-robot fa-3x";
-      faShareIcon = "icon share fas fa-rocket fa-3x";
-
-      $commentIcon = $('<i></i>');
-      $commentIcon.attr({ class: faCommentIcon });
-      $retweetIcon = $('<i></i>');
-      $retweetIcon.attr({ class: faRetweetIcon });
-      $likeIcon = $('<i></i>');
-      $likeIcon.attr({ class: faLikeIcon });
-      $shareIcon = $('<i></i>');
-      $shareIcon.attr({ class: faShareIcon });
-
-
-      //text updates & inputs
-      $message.text(tweet.message);
-      $username.text('@' + tweet.user);
-      $timestamp.text(jQuery.timeago(tweet.created_at))
-
-      //main tweet DIV
-      $tweet.appendTo($feed); //was app
-      //sub DIVs
-      $profilePhoto.appendTo($tweet);
-      $timestamp.appendTo($tweet);
-      $username.appendTo($tweet);
-      $message.appendTo($tweet);
-      $commentIcon.appendTo($tweet);
-      $retweetIcon.appendTo($tweet);
-      $likeIcon.appendTo($tweet);
-      $shareIcon.appendTo($tweet);
-
-      $username.on("click", function (event) {
-        console.log(event, event.currentTarget.innerText);
-        alert("Button has been pressed " + event.currentTarget.innerText);
-        document.querySelector('button').innerText = "Back";
-        var currUser = event.currentTarget.innerText.slice(1);
-        userFeed(currUser);
-      })
-      index -= 1;
-    }
-  }
-
-
-  var userFeed = function (user) {
-    $feed.html('');
-    var index = streams.users[user].length - 1;
-
-    while (index >= 0) {
-      var tweet = streams.users[user][index];
+      //var tweet = streams[args][index];
+      var tweet = base[index]
+      //console.log(tweet)
       var $tweet = $('<div class="tweet"></div>');
       var $message = $('<div class="message"></div>');
       var $username = $('<div class="username"></div>');
-      var $timestamp = $('<div class="timestamp"></div><br>');
+      var $timestamp = $('<div class="timestamp"></div>');
+      var $iconbar = $('<nav id="mainNavBar" class="navbar navbar-dark navbar-expand-md py-0"></nav>')
 
       //profile photo
-      var imgsrc = tweet.profilePhotoURL
-
+      var imgsrc = base[index].profilePhotoURL;
       $profilePhoto = $('<img>');
       $profilePhoto.attr({ src: imgsrc, class: "profile-photo" });
 
       //icons
-      faCommentIcon = "icon comment fas fa-thumbs-up fa-3x";
-      faRetweetIcon = "icon retweet fas fa-satellite-dish fa-3x";
-      faLikeIcon = "icon like fas fa-robot fa-3x";
-      faShareIcon = "icon share fas fa-rocket fa-3x";
+      faCommentIcon = "icon comment fas fa-thumbs-up fa-2x";
+      faRetweetIcon = "icon retweet fas fa-satellite-dish fa-2x";
+      faLikeIcon = "icon like fas fa-robot fa-2x";
+      faShareIcon = "icon share fas fa-rocket fa-2x";
 
       $commentIcon = $('<i></i>');
       $commentIcon.attr({ class: faCommentIcon });
@@ -122,31 +64,41 @@ $(document).ready(function () {
       $tweet.appendTo($feed);
       //sub DIVs
       $profilePhoto.appendTo($tweet);
-      $timestamp.appendTo($tweet);
       $username.appendTo($tweet);
       $message.appendTo($tweet);
-      $commentIcon.appendTo($tweet);
-      $retweetIcon.appendTo($tweet);
-      $likeIcon.appendTo($tweet);
-      $shareIcon.appendTo($tweet);
+      $timestamp.appendTo($tweet);
 
+      $iconbar.appendTo($tweet);
+      $commentIcon.appendTo($iconbar);
+      $retweetIcon.appendTo($iconbar);
+      $likeIcon.appendTo($iconbar);
+      $shareIcon.appendTo($iconbar);
+
+      // $username.on("click", function (event) {
+      //   document.querySelector('button').innerText = "Back";
+      //   var currUser = event.currentTarget.innerText.slice(1);
+      //   renderFeed(currUser)
+      // })
       index -= 1;
     }
-
-    $button.on("click", function (event) {
-      document.querySelector('button').innerText = "Update Feed";
-      renderFeed()
-    })
   }
 
   //Set event listeners
   $title.on("click", handleTitleClick);
   $subtitle.on("click", handleTitleClick);
-  $button.on("click", renderFeed);
-
+  $app.on("click", "button", function (event) {
+    console.dir(event)
+    event.target.innerText = "Update Feed"
+    renderFeed()
+  })
+  $feed.on("click", "div", function (event) {
+    document.querySelector('button').innerText = "Back";
+    var currUser = event.target.innerText.slice(1);
+    renderFeed(currUser)
+  })
 
   //Append new HTMl elements to the DOM
-  $app.html(''); //removes any html from the page
+  $app.html('');
   $title.appendTo($app);
   $subtitle.appendTo($app);
   $button.appendTo($app);
@@ -154,7 +106,6 @@ $(document).ready(function () {
   renderFeed();
 
 });
-
 
 
 window.isItBeautifulYet = true;
