2f146bdbbc4b91519097e554daaef8d147f454a5
Yanick <yanick@bu.edu>
2022-02-02 16:26:08 -0600

    code re-factored and CSS added

--- app.js
+++ app.js
@@ -12,17 +12,16 @@ $(document).ready(function(){
 // Create new HTML elements ----------------
   var $homeFeedSection = $ ('<section id="feed"></section>');
   var $buttonDiv = $ ('<div class="buttons"></div>');
-  var $homeFeedButton = $ ('<button id = "update-feed">Update Feed</button>');
+  var $homeFeedButton = $ ('<button id = "update-feed">UPDATE  FEED</button>');
   var $tweetDiv = $ ('<div class="flexbox-container"></div>');
 
 // Helper & event handler functions ------------------
 
 // Set event listeners (providing appropriate handlers as input)
 $homeFeedButton.on("click", function(event) {
-  renderFeed();
+  renderUserFeed();
   return event;
 });
-
 
 $(document).on("click", ".username", function() {
   $tweetDiv.empty();
@@ -33,9 +32,8 @@ $(document).on("click", ".username", function() {
 
 $(document).on("click", "#update-feed", function() {
   if (this.innerHTML === "BACK");
-  $("#update-feed").text('Update Feed');
+  $("#update-feed").text('UPDATE FEED');
 })
-
 
 //----RENDER USER FEED FUN -------
 
@@ -52,7 +50,7 @@ $(document).on("click", "#update-feed", function() {
     for (var i = 0; i < streams.home.length; i++) {
       var fullTweet = streams.home[i].message;
       var userHandle = '@' + streams.home[i].user;
-        if (seen[fullTweet] === undefined && userInQuestion === userHandle) {
+        if (seen[fullTweet] === undefined && (userInQuestion === userHandle || userInQuestion === undefined)) {
 
           var $newTweet = $ ('<div class="flexbox-container tweet"></div>');
           $newTweet.text('');
@@ -68,7 +66,7 @@ $(document).on("click", "#update-feed", function() {
           $newTweetMessage.text(streams.home[i].message)
           $newTweetMessage.appendTo($newTweet);
 
-          var $icons = $ ('<span class="icon"style="font-size: 1em; color: Tomato;">');
+          var $icons = $ ('<span class="icon"style="font-size: 1em; color: #62B16E;">');
           var $comment = $ ('<i class="icon comment far fa-comment"></i>');
           $comment.appendTo($icons);
           var $retweet = $ ('<i class="icon retweet fas fa-retweet"></i>');
@@ -92,81 +90,7 @@ $(document).on("click", "#update-feed", function() {
 
   }
 
-  //-------RENDER FEED FUNCTION -----------
-
-  // REFRESH ITERATION NOT YET WORKING IN THIS FUNCTION.
-  var renderFeed = function(clickInupt) {
-    var seen = {};
-    $('.message').each(function() {
-      var message = $(this).text();
-      if (seen[message]) {
-        $(this).remove();
-      } else {
-        seen[message] = true;
-      }
-    });
-
-    for (var i = 0; i < streams.home.length; i++) {
-      var fullTweet = streams.home[i].message;
-        if (seen[fullTweet] === undefined) {
-
-          var $newTweet = $ ('<div class="flexbox-container tweet"></div>');
-          $newTweet.text('');
-
-          var $image = $ ('<img class="profile-photo" + <img src="./assets/img/' + streams.home[i].user + '.png">'+ '</img>');
-          $image.appendTo($newTweet);
-
-          var $newUser = $ ('<div class="username"></div>');
-          $newUser.text('@' + streams.home[i].user);
-          $newUser.appendTo($newTweet);
-
-          var $newTweetMessage = $ ('<p class="message"></p>');
-          $newTweetMessage.text(streams.home[i].message)
-          $newTweetMessage.appendTo($newTweet);
-
-          var $icons = $ ('<span class="icon"style="font-size: 1.5em; color: green;">');
-          var $comment = $ ('<i class="icon comment far fa-comment"></i>');
-          $comment.appendTo($icons);
-          var $retweet = $ ('<i class="icon retweet fas fa-retweet"></i>');
-          $retweet.appendTo($icons);
-          var $like = $ ('<i class="icon like fas fa-thumbs-up"></i>');
-          $like.appendTo($icons);
-          var $share = $ ('<i class="icon share fas fa-share"></i>');
-          $share.appendTo($icons);
-          $icons.appendTo($newTweet);
-
-          var $timeStamp = $ ('<div class="timestamp"></div>');
-          $timeStamp.appendTo($newTweet);
-          $timeStamp.text(jQuery.timeago(streams.home[i].created_at));
-
-          $newTweet.prependTo($tweetDiv);
-          $tweetDiv.appendTo($homeFeedSection);
-
-
-
-          // $newUser.on ("click", function(event) {
-          //   $homeFeedButton.text('BACK');
-          //   var theUser = event.target.innerText /* WILL RETURN USERNAME.. PASS INTO USER FEED FUNCTION */
-          //   // $newUser.html('');
-          //   // $newTweetMessage.text('');
-          //   $tweetDiv.empty();
-          //   //EMPTY IS WORKING, RE-RENDER IS NOT
-          //   rendeUserFeed(event, theUser);
-          // });
-
-        }
-
-
-
-    }
-
-
-  }
-
-  //renderUserFeed();
-  renderFeed();
-
-
+  renderUserFeed();
 
 // Append new HTML elements to the DOM
 
@@ -174,10 +98,6 @@ $(document).on("click", "#update-feed", function() {
   $buttonDiv.appendTo($homeFeedSection);
   $homeFeedButton.appendTo($buttonDiv);
   $tweetDiv.appendTo($homeFeedSection);
-  //$tweetMessage.appendTo('.tweet');
-
-
-
 
 });
 
