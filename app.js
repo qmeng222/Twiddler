f2a15894f8289ec5aa05a910e04c38c720e3e048
Yanick <yanick@bu.edu>
2022-02-02 10:57:16 -0600

    pass test 36

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ $homeFeedButton.on("click", function(event) {
 $(document).on("click", ".username", function() {
   $tweetDiv.empty();
   $("#update-feed").text('BACK');
-  renderFeed();
+  var clickedUser = this.innerHTML;
+  //alert(clickedUser);
+  renderUserFeed(clickedUser);
 
   //feed button needs to be included in render!!
 })
@@ -36,62 +38,61 @@ $(document).on("click", ".username", function() {
 
 //----RENDER USER FEED FUN -------
 
-  // var renderUserFeed = function(userInQuestion) {
-  //   var seen = {};
-  //   $('.message').each(function() {
-  //     var message = $(this).text();
-  //     if (seen[message]) {
-  //       $(this).remove();
-  //     } else {
-  //       seen[message] = true;
-  //     }
-  //   });
+  var renderUserFeed = function(userInQuestion) {
+    var seen = {};
+    $('.message').each(function() {
+      var message = $(this).text();
+      if (seen[message]) {
+        $(this).remove();
+      } else {
+        seen[message] = true;
+      }
+    });
 
-  //   for (var i = 0; i < streams.home.length; i++) {
-  //     var fullTweet = streams.home[i].message;
-  //     var userHandle = '@' + streams.home[i].username + ': ';
-  //     //console.log(userHandle);
-  //       if (seen[fullTweet] === undefined && userHandle === userInQuestion) {
-  //         var newTweetObj = streams.home[i];
+    for (var i = 0; i < streams.home.length; i++) {
+      var fullTweet = streams.home[i].message;
+      var userHandle = '@' + streams.home[i].user;
+      //console.log(userHandle);
+      console.log($('userInQuestion'));
+        if (seen[fullTweet] === undefined && userInQuestion === userHandle) {
 
-  //         var $newTweet2 = $ ('<div class="tweet"></div>');
-  //         $newTweet2.text('');
-  //         $newTweet2.text('');
+          var $newTweet = $ ('<div class="tweet"></div>');
+          $newTweet.text('');
 
-  //         var $image2 = $ ('<img class="profile-photo"src="assets/img/>' + newTweetObj.user + '.png">');
-  //         $image2.appendTo($newTweet2);
+          var $image = $ ('<img class="profile-photo" + <img src="./assets/img/' + streams.home[i].user + '.png">'+ '</img>');
+          $image.appendTo($newTweet);
 
-  //         var $newUser2 = $ ('<div class="username"></div>');
-  //         $newUser2.text('');
-  //         $newUser2.text('@' + newTweetObj.user + ':' );
-  //         $newUser2.appendTo($newTweet2);
+          var $newUser = $ ('<div class="username"></div>');
+          $newUser.text('@' + streams.home[i].user);
+          $newUser.appendTo($newTweet);
 
-  //         var $newTweetMessage2 = $ ('<p class="message"></p>');
-  //         $newTweetMessage2.html('');
-  //         $newTweetMessage2.text(streams.home[i].message)
-  //         $newTweetMessage2.appendTo($newTweet2);
+          var $newTweetMessage = $ ('<p class="message"></p>');
+          $newTweetMessage.text(streams.home[i].message)
+          $newTweetMessage.appendTo($newTweet);
 
-  //         var $icons2 = $ ('<span class="icon"style="font-size: 1em; color: Tomato;">');
-  //         var $comment2 = $ ('<i class="icon comment far fa-comment"></i>');
-  //         $comment2.appendTo($icons2);
-  //         var $retweet2 = $ ('<i class="icon retweet fas fa-retweet"></i>');
-  //         $retweet2.appendTo($icons2);
-  //         var $like2 = $ ('<i class="icon like fas fa-thumbs-up"></i>');
-  //         $like2.appendTo($icons2);
-  //         var $share2 = $ ('<i class="icon share fas fa-share"></i>');
-  //         $share2.appendTo($icons);
-  //         $icons2.appendTo($newTweet2);
+          var $icons = $ ('<span class="icon"style="font-size: 1em; color: Tomato;">');
+          var $comment = $ ('<i class="icon comment far fa-comment"></i>');
+          $comment.appendTo($icons);
+          var $retweet = $ ('<i class="icon retweet fas fa-retweet"></i>');
+          $retweet.appendTo($icons);
+          var $like = $ ('<i class="icon like fas fa-thumbs-up"></i>');
+          $like.appendTo($icons);
+          var $share = $ ('<i class="icon share fas fa-share"></i>');
+          $share.appendTo($icons);
+          $icons.appendTo($newTweet);
 
-  //         var $timeStamp2 = $ ('<div class="timestamp"></div>');
-  //         $timeStamp2.appendTo($newTweet2);
-  //         $timeStamp2.text(jQuery.timeago(streams.home[i].created_at));
-  //         orderCorrectly($newTweet2);
+          var $timeStamp = $ ('<div class="timestamp"></div>');
+          $timeStamp.appendTo($newTweet);
+          $timeStamp.text(jQuery.timeago(streams.home[i].created_at));
 
-  //       }
+          $newTweet.prependTo($tweetDiv);
+          $tweetDiv.appendTo($homeFeedSection);
 
-  //   }
-  //   return renderUserFeed;
-  // }
+        }
+
+    }
+
+  }
 
   //-------RENDER FEED FUNCTION -----------
 
@@ -110,18 +111,15 @@ $(document).on("click", ".username", function() {
     for (var i = 0; i < streams.home.length; i++) {
       var fullTweet = streams.home[i].message;
         if (seen[fullTweet] === undefined) {
-          var newTweetObj = streams.home[i];
-
-
 
           var $newTweet = $ ('<div class="tweet"></div>');
           $newTweet.text('');
 
-          var $image = $ ('<img class="profile-photo" + <img src="./assets/img/' + newTweetObj.user + '.png">'+ '</img>');
+          var $image = $ ('<img class="profile-photo" + <img src="./assets/img/' + streams.home[i].user + '.png">'+ '</img>');
           $image.appendTo($newTweet);
 
           var $newUser = $ ('<div class="username"></div>');
-          $newUser.text('@' + newTweetObj.user + ':' );
+          $newUser.text('@' + streams.home[i].user);
           $newUser.appendTo($newTweet);
 
           var $newTweetMessage = $ ('<p class="message"></p>');
@@ -160,12 +158,16 @@ $(document).on("click", ".username", function() {
 
         }
 
+
+
     }
 
 
   }
 
+  //renderUserFeed();
   renderFeed();
+
 
 
 // Append new HTML elements to the DOM
