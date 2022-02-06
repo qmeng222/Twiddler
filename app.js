d6d82e009d0ef9eb7575dd75b110ebcbda5cd850
Shloka <shlokareddy@gmail.com>
2022-02-05 18:34:05 -0600

    refactored app.js code

--- app.js
+++ app.js
@@ -2,7 +2,45 @@ $(document).ready(function(){
   var $app = $('#app');
   $app.html('');
 
-  // Adds twiddler page heading
+  // Select already existing elements
+
+  // Create new HTML elements
+
+  var $title = $('<h1>twiddler<h1>');
+  var $update_feed_btn = $('<button id="update-feed">update feed</button>');
+  var $home_feed = $('<div id="feed"></div>');
+
+  // Create event handler functions
+
+  var handleTitleClick = function(event) {
+    alert('The title of the page is: ' + event.target.innerText);
+  };
+
+  var renderFeed = function(event) {
+    var oldFeedLength = $('div.tweet').length;
+    var newFeedLength = streams.home.length;
+    for (var i = oldFeedLength; i < newFeedLength; i++) {
+      var tweet = streams.home[i];
+      var $tweet = $('<div class="tweet"></div>');
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      $tweet.prependTo($home_feed);
+    }
+  };
+  renderFeed(); //create initial home feed
+
+  // Set event listeners
+
+  $title.on('click', handleTitleClick);
+  $update_feed_btn.on('click', renderFeed);
+
+  // Append new HTML elements to the DOM
+
+  $title.appendTo($app);
+  $app.append($update_feed_btn);
+  $app.append($home_feed);
+
+
+ /* // Adds twiddler page heading
   var $title = $('<h1>twiddler<h1>');
   $title.appendTo($app);
 
@@ -20,6 +58,19 @@ $(document).ready(function(){
   var $home_feed = $('<div id="feed"></div>');
   $app.append($home_feed);
 
+
+  //
+  var renderFeed = function(event) {
+    var oldFeedLength = $('div.tweet').length;
+    var newFeedLength = streams.home.length;
+    for (var i = oldFeedLength; i < newFeedLength; i++) {
+      var tweet = streams.home[i];
+      var $tweet = $('<div class="tweet"></div>');
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      $tweet.prependTo($home_feed);
+    }
+  };
+
   // Adds initial tweets to home feed
   var index = streams.home.length - 1;
   while(index >= 0){
@@ -31,15 +82,6 @@ $(document).ready(function(){
   }
 
   // Click event for update feed button
-  $update_feed_btn.on('click', function(event) {
-    var oldFeedLength = $('div.tweet').length;
-    var newFeedLength = streams.home.length;
-    for (var i = oldFeedLength; i < newFeedLength; i++) {
-      var tweet = streams.home[i];
-      var $tweet = $('<div class="tweet"></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message);
-      $tweet.prependTo($home_feed);
-    }
-  });
+  $update_feed_btn.on('click', renderFeed);
 
-});
+*/});
