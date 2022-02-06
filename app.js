e1dd6d8d5a2feda7cf194c5f58d35aac77fa44a7
Shloka <shlokareddy@gmail.com>
2022-02-05 22:04:59 -0600

    tweet ui component

--- app.js
+++ app.js
@@ -20,10 +20,38 @@ $(document).ready(function(){
     var oldFeedLength = $('div.tweet').length;
     var newFeedLength = streams.home.length;
     for (var i = oldFeedLength; i < newFeedLength; i++) {
+      //adds tweet division container
       var tweet = streams.home[i];
       var $tweet = $('<div class="tweet"></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message);
       $tweet.prependTo($home_feed);
+      //adds profile pic child element
+      var $profile_pic = $('<img class="profile-photo">');
+      $profile_pic.attr('src', tweet.profilePhotoURL);
+      $profile_pic.appendTo($tweet);
+      //adds username child element
+      var $username = $('<span class="username"></span');
+      $username.text('@' + tweet.user);
+      $username.appendTo($tweet);
+      //adds message child element
+      var $message = $('<span class="message"></span>');
+      $message.text(tweet.message);
+      $message.appendTo($tweet);
+      //adds timestamp child element
+      var $timestamp = $('<span class="timestamp"></span>');
+      $timestamp.text(tweet.created_at);
+      $timestamp.appendTo($tweet);
+      //adds comment icon
+      var $comment = $('<img class="comment" src="./assets/icons/placeholder.png">');
+      $comment.appendTo($tweet);
+      //adds retweet icon
+      var $retweet = $('<img class="retweet" src="./assets/icons/placeholder.png">');
+      $retweet.appendTo($tweet);
+      //adds like icon
+      var $like = $('<img class="like" src="./assets/icons/placeholder.png">');
+      $like.appendTo($tweet);
+      //adds share icon
+      var $share = $('<img class="share" src="./assets/icons/placeholder.png">');
+      $share.appendTo($tweet);
     }
   };
   renderFeed(); //create initial home feed
@@ -72,14 +100,7 @@ $(document).ready(function(){
   };
 
   // Adds initial tweets to home feed
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div class="tweet"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($home_feed);
-    index -= 1;
-  }
+  renderFeed();
 
   // Click event for update feed button
   $update_feed_btn.on('click', renderFeed);
