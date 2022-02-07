2272beacda4e549c8838e2ab437dbd15ca639c6d
Paul <Paul.s.koh@gmail.com>
2022-02-07 18:28:20 -0500

    Finished up to UserFeed

--- app.js
+++ app.js
@@ -12,22 +12,49 @@ $(document).ready(function(){
 
 
 //Creates event handler functions
-  var renderFeed = function() {
+
+  var handleTitleClick = function(event) {
+    alert('The title of this page is : ' + event.target.innerText);
+  };
+
+  var handleUpdateClick = function(event) {
+    $updateFeedButton.text('Update Feed');
+    $('.tweet').remove();
+    renderFeed();
+  }
+
+  var handleUsernameClick = function(event) {
+    var name = event.target.innerText;
+    var username = name.slice(1);
+    $('.tweet').remove();
+    $updateFeedButton.text('Back');
+    renderFeed(username);
+  }
+
+  var renderFeed = function(username) {
     $homeFeed.empty();
-    var index = streams.home.length - 1;
+
+    if (username) {
+      var tweetArray = streams.users[username];
+    } else {
+      var tweetArray = streams.home;
+    }
+
+    var index = tweetArray.length - 1;
 
     while(index >= 0){
-      var tweet = streams.home[index];
+      var tweet = tweetArray[index];
       var $tweet = $('<div class="tweet"></div>');
+
       //Declaring all components of Tweet variables
       var $profile = $('<img class= "profile-photo" src="./assets/img/' + tweet.user + '.png"></img>');
       var $username = $('<div class= "username"></div>');
       var $message = $('<p class= "message"></p>');
-      var $timestamp = $('<div class= "timestamp"></div>');
+      var $timestamp = $('<div class= "timestamp">' + jQuery.timeago(tweet.created_at) + '</div>');
       var $comment = $('<i class= "icon comment fas fa-comment-alt"></i>');
       var $retweet = $('<i class= "icon retweet fas fa-retweet"></i>');
-      var $like = $('<i class= "icon like fab fa-gratipay"></i>');
-      var $share = $('<i class= "icon share fas fa-share-square"></i>');
+      var $like = $('<i class= "icon like fas fa-heart"></i>');
+      var $share = $('<i class= "icon share fas fa-share"></i>');
 
       //Formating each element to fit requirement
       $message.text(tweet.message);
@@ -38,26 +65,37 @@ $(document).ready(function(){
       $tweet.append($username);
       $tweet.append($profile);
       $tweet.append($message);
+      $tweet.append($timestamp);
       $tweet.append($comment);
       $tweet.append($retweet);
+      $tweet.append($like);
       $tweet.append($share);
 
       //Adding all Tweets to Home Feed
       $tweet.appendTo($homeFeed);
       index -= 1;
+
+      $username.on('click', handleUsernameClick);
     }
   };
 
   renderFeed();
 
+
 //Creates event listeners functions
-$updateFeedButton.on('click', renderFeed);
+$updateFeedButton.on('click', function () {
+  $updateFeedButton.text() === "Back" ? $updateFeedButton.text('Update Feed') : null;
+  renderFeed();
+});
 
 
 //Append new HTML elements to the DOM
 $title.appendTo($app);
 $updateFeedButton.appendTo($app);
 $homeFeed.appendTo($app);
+
+
+
 
 
 });
