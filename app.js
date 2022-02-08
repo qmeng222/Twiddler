477aac19a6f32a95ee5bddf47959a0aca1fd6987
Alex Yeung <a.yeung608@yahoo.com>
2022-02-08 15:46:23 -0800

    "Updated format and code"

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
   // Create new HTML elements
   var $title = $('<h1>Twiddler</h1>');
   var $subtitle = $('<h2>Where Twiddlers be Twiddlin\'</h2>');
-  var $updateButton = $('<button id="update-feed" onclick=updateFeed()>Update Feed</button>');
-  var $backButton = $('<button id="back" onclick=goBack() style="display:none;">Back</button>');
+  var $updateButton = $('<button id="update-feed">Update Feed</button>');
+  var $feed = $('<div id="feed" class="feed"></div>');
   // var $feedDiv = $('<div id="feed" class="feed"></div>');
   var $app = $('.app');
   // Create event handler functions
@@ -21,27 +21,22 @@ $(document).ready(function () {
   // Append new HTML elements to the DOM
   //$app.appendTo($body);
   $updateButton.prependTo($app);
-  $backButton.prependTo($app);
+  $feed.appendTo($app);
   $subtitle.prependTo($body);
   $title.prependTo($body);
 
   //$feedDiv.appendTo($app);
 
   renderFeed();
+  window.isItBeautifulYet = true;
 });
 
 
-
-var updateFeed = function () {
-  streams.home = [];
-  for (var i = 0; i < 11; i++) {
-    generateRandomTweet();
-  }
-  renderFeed();
-};
-
 var renderFeed = function () {
   var $feed = $('.feed');
+  $('#update-feed').text('Update Feed');
+  $('#update-feed').attr('onClick', 'renderFeed()');
+  $feed.empty();
   var index = 0;
   while (index < streams.home.length) {
     var tweet = streams.home[index];
@@ -68,19 +63,40 @@ var renderFeed = function () {
 };
 
 var filterUser = function (arg) {
-  $('#update-feed').hide();
-  $('#back').show();
-  var tweets = $('#feed').children('.tweet').each(function () {
-    if ($(this).children('.username').text() !== arg) {
-      $(this).hide();
+  var $feed = $('.feed');
+  $feed.empty();
+  var index = 0;
+  $('#update-feed').text('Back');
+
+  while (index < streams.home.length) {
+
+    var tweet = streams.home[index];
+    var username = '@' + tweet.user;
+    if (username === arg) {
+      var $tweet = $('<div class="tweet"></div>');
+      var $username = $('<div class="username" onclick="filterUser(this.innerHTML)"></div>');
+      var $pic = $('<br><img class="profile-photo"></img>');
+      var $timestamp = $('<div class="timestamp"></div>');
+      var $message = $('<div class="message"></div>');
+      var $icons = $('<i class="fas fa-comments comment"></i> <i class="fas fa-retweet retweet"></i> <i class="fas fa-thumbs-up like"></i> <i class="fas fa-share share"></i><br>');
+
+      $username.text(username);
+      $message.text(tweet.message);
+      $timestamp.text($.timeago(tweet.created_at));
+      $pic.attr('src', tweet.profilePhotoURL);
+      $tweet.prependTo($feed);
+      $icons.prependTo($tweet);
+      $timestamp.prependTo($tweet);
+      $message.prependTo($tweet);
+      $username.prependTo($tweet);
+      $pic.prependTo($tweet);
     }
-  });
+    index += 1;
+  }
 };
 
 var goBack = function () {
   $('#back').hide();
   $('#update-feed').show();
-  var tweets = $('#feed').children('.tweet').each(function () {
-    $(this).show();
-  });
+  renderFeed();
 };
