98382501cb485bc9f5055097b1d104bcd96b10de
calvinfsee <calvinfrancissee@gmail.com>
2022-02-11 04:50:27 -0800

    Implemented Extra Credit

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
+var visitor;
 $(document).ready(function(){
   var $app = $('#app');
   $app.html('');
 
-  var $header = $('<header></header>'); //Header is added to page
-  $header.appendTo($app);
-  $('<button class="interactive heading" id="update-feed">Update Feed</button>').appendTo($header);
-  $('<h1 class="heading">Twiddler</h1>').appendTo($header);
 
-  // $('<div class="heading" id="sign-in"><h3 class="new-user">Sign in here!</h3><input class="register"><button class="interactive heading" id="submit">LOG<br>IN</button></div>').appendTo($header);
+  $('<header></header>').appendTo($app); //Header is added to page
+  $('<button class="interactive heading" id="update-feed">Update Feed</button>').appendTo('header');
+  $('<h1 class="heading">Twiddler</h1>').appendTo('header');
 
 
 
   $('<div id="feed"></div>').appendTo($app); //Feed is added to page
+  $('<div id="sidebars"></div>').appendTo($app);
   var $feed = $('#feed');
+  var $sidebars = $('#sidebars');
 
-  $('<nav class="widget" id="friends"><h2>Friend List</h2><ul id="friendlist"></ul></nav').appendTo($app); //Friend list is added to page
-  var $friendlist = $('#friendlist');
-  $('<li class="friend interactive">shawndrost</li>').appendTo($friendlist);
-  $('<li class="friend interactive">sharksforcheap</li>').appendTo($friendlist);
-  $('<li class="friend interactive">mracus</li>').appendTo($friendlist);
-  $('<li class="friend interactive">douglascalhoun</li>').appendTo($friendlist);
+  var renderSidebar = function() {
+    $sidebars.html('');
+    $('<nav class="sidebar" id="friends"><h2>Friend List</h2><ul id="friendlist"></ul></nav').appendTo($sidebars); //Renders friend list
+    var $friendlist = $('#friendlist');
+    for (var key in streams.users) {
+      $('<li class="friend interactive">' + key + '</li>').appendTo($friendlist);
+    }
 
-  $('<form id="new-tweet-form"></form>').appendTo($app);
-  var $newTweet = $('#new-tweet-form');
-  $('<label for="username">Username</label>').appendTo($newTweet);
-  $('<input name="username">').appendTo($newTweet);
-  $('<label for="message">Tweet Message</label>').appendTo($newTweet);
-  $('<input name="message">').appendTo($newTweet);
-  $('<input type="submit" class="interactive" id="send-tweet" value="Post">').appendTo($newTweet);
+    var $newTweet = $('<form class="sidebar" id="new-tweet-form"></form>'); //Renders new tweet form
+    $('<label for="username">Username</label>').appendTo($newTweet);
+    $('<input name="username" id="username">').appendTo($newTweet);
+    $('<label for="message">Tweet Message</label>').appendTo($newTweet);
+    $('<input name="message" id="new-message">').appendTo($newTweet);
+    $('<input type="submit" class="interactive" id="send-tweet" value="Post Tweet">').appendTo($newTweet);
 
+    $newTweet.appendTo($sidebars);
+
+    $( "#new-tweet-form" ).submit(function(event) {
+      event.preventDefault();
+      visitor = $('#username').val();
+      var message = $('#new-message').val();
+      writeTweet(message);
+      homeFeed();
+      renderSidebar();
+    });
+    $('li.friend').click(function() {
+      var user = $(this).text();
+      user = streams.users[user];
+      renderFeed(user);
+    });
+  };
 
   var renderFeed = function(userFeed) {
     $feed.html('');
@@ -39,20 +56,14 @@ $(document).ready(function(){
       $('#update-feed').html('Back');
     }
     for (var i = 0; i < userFeed.length; i++) {
-      var tweet = userFeed[i]; //Tweet format is created
-      var $tweet = $('<div class="tweet"></div>');
+      var tweet = userFeed[i];
+      var $tweet = $('<div class="tweet"></div>'); //Tweet format is created
       var $tweetData = $('<div id="tweetData"></div>');
-      var $message = $('<p class="message"></p>');
       var $icons = $('<div id="icon-container"></div>');
 
-      $('<img class="user-data profile-photo" src="' + tweet.profilePhotoURL + '">').appendTo($tweetData); //User data is parsed here
-      $('<span class="user-data username interactive">@' + tweet.user + '</span>').appendTo($tweetData);
+      $('<img class="user-data profile-photo" src="' + tweet.profilePhotoURL + '">').appendTo($tweetData); //Tweet data is parsed
+      $('<span class="user-data friend interactive">@' + tweet.user + '</span>').appendTo($tweetData);
       $('<span class="timestamp">' + jQuery.timeago(tweet.created_at) + '</span>').appendTo($tweetData);
-
-
-      //Add timestamp here
-
-      $message.text(tweet.message); //message and timestap are parsed here
 
       $('<i class="fa-solid fa-comment icons comment"></i>').appendTo($icons); //interaction icons are added
       $('<i class="fa-solid fa-retweet icons retweet"></i>').appendTo($icons);
@@ -60,52 +71,30 @@ $(document).ready(function(){
       $('<i class="fa-solid fa-share-from-square icons share"></i>').appendTo($icons);
 
       $tweetData.appendTo($tweet); //Parsed data input into tweet format
-      $message.appendTo($tweet);
+      $('<p class="message">' + tweet.message + '</p>').appendTo($tweet);
       $icons.appendTo($tweet);
-
-
-
-      $tweet.prependTo($feed); //Tweet added to feed
+      $tweet.prependTo($feed); //Tweet added to top of feed
     }
-
   }
-  var homeFeed = function() {
+
+  var homeFeed = function () {
     renderFeed();
-    $('.user-data').click(function() {
-      var user = $(this).parent().find('.username').text().substring(1);
+    $('span.user-data').click(function () {
+      var user = $(this).parent().find('.friend').text().substring(1);
       user = streams.users[user];
       renderFeed(user);
     });
-  }
+  };
+  renderSidebar();
   homeFeed();
-  $("#update-feed").click(function() {
+
+
+  $("#update-feed").click(function () {
     homeFeed();
   });
-  $('.friend').click(function() {
-    var user = $(this).text();
-    user = streams.users[user];
-    renderFeed(user);
-  });
 
 
 
 
-  //function newUser
-  //On submit form, invoke function
-  //set local var newUser to input
-  //if newUser is undefined, alert no input
-  //set global currentUser variable to local variable
-  //streams.user[currentUser] = [];
-  //append currentUser to friends list with class "friend user"
-
-  //function sentTweet
-  //On submit form, invoke function
-  //if no user/input, alert
-  //var newTweet = {}
-  //tweet.user = currentUser
-  //tweet.message = input
-  //tweet.created_at = new Date();
-  //tweet.profilePhotoURL = default pic;
-  //addTweet(newTweet);
   window.isItBeautifulYet = true;
 });
