a5ec21fd2abc3439a3926a0f9507152c973d70cf
calvinfsee <calvinfrancissee@gmail.com>
2022-02-11 10:24:46 -0800

    Refined code to run more efficiently

--- app.js
+++ app.js
@@ -3,17 +3,25 @@ $(document).ready(function(){
   var $app = $('#app');
   $app.html('');
 
-
   $('<header></header>').appendTo($app); //Header is added to page
-  $('<button class="interactive heading" id="update-feed">Update Feed</button>').appendTo('header');
+  $('<button class="interactive heading" id="update-feed">Update</button>').appendTo('header');
+  $('<h1 class="heading interactive" id="current-feed">Home Feed</h1>').appendTo('header');
   $('<h1 class="heading">Twiddler</h1>').appendTo('header');
-
-
 
   $('<div id="feed"></div>').appendTo($app); //Feed is added to page
   $('<div id="sidebars"></div>').appendTo($app);
   var $feed = $('#feed');
   var $sidebars = $('#sidebars');
+
+  var newTweetTemplate = (function () {
+    var $newTweet = $('<form class="sidebar" id="new-tweet-form"></form>'); //new tweet template
+    $('<label for="username">Username</label>').appendTo($newTweet);
+    $('<input name="username" id="username">').appendTo($newTweet);
+    $('<label for="message">Tweet Message</label>').appendTo($newTweet);
+    $('<input name="message" id="new-message">').appendTo($newTweet);
+    $('<input type="submit" class="interactive" id="send-tweet" value="Post Tweet">').appendTo($newTweet);
+    return $newTweet;
+  })();
 
   var renderSidebar = function() {
     $sidebars.html('');
@@ -23,65 +31,67 @@ $(document).ready(function(){
       $('<li class="friend interactive">' + key + '</li>').appendTo($friendlist);
     }
 
-    var $newTweet = $('<form class="sidebar" id="new-tweet-form"></form>'); //Renders new tweet form
-    $('<label for="username">Username</label>').appendTo($newTweet);
-    $('<input name="username" id="username">').appendTo($newTweet);
-    $('<label for="message">Tweet Message</label>').appendTo($newTweet);
-    $('<input name="message" id="new-message">').appendTo($newTweet);
-    $('<input type="submit" class="interactive" id="send-tweet" value="Post Tweet">').appendTo($newTweet);
+    newTweetTemplate.clone().appendTo($sidebars);
 
-    $newTweet.appendTo($sidebars);
-
-    $( "#new-tweet-form" ).submit(function(event) {
+    $( "#new-tweet-form" ).click(function(event) { //write a new tweet
       event.preventDefault();
       visitor = $('#username').val();
-      var message = $('#new-message').val();
-      writeTweet(message);
+      writeTweet($('#new-message').val());
       homeFeed();
       renderSidebar();
     });
-    $('li.friend').click(function() {
+
+    $('li.friend').click(function() { //Renders user feed from friend list
       var user = $(this).text();
-      user = streams.users[user];
       renderFeed(user);
     });
   };
 
-  var renderFeed = function(userFeed) {
+  var tweetIcons = (function () { //Creates icon container object
+    var $icons = $('<div id="icon-container"></div>');
+    $('<i class="fa-solid fa-comment icons comment"></i>').appendTo($icons);
+    $('<i class="fa-solid fa-retweet icons retweet"></i>').appendTo($icons);
+    $('<i class="fa-solid fa-heart icons like"></i>').appendTo($icons);
+    $('<i class="fa-solid fa-share-from-square icons share"></i>').appendTo($icons);
+    console.log(typeof $icons);
+    return $icons;
+  })();
+
+  var renderFeed = function(username) {
     $feed.html('');
-    if(userFeed === undefined) { //If no user selected, default renders home feed
+    var userFeed;
+    if(username === undefined) { //If no user selected, default renders home feed
       userFeed = streams.home;
-      $('#update-feed').html('Update Feed');
+      $('#update-feed').html('Update');
+      $('#current-feed').html('Home');
     } else {
       $('#update-feed').html('Back');
+      $('#current-feed').html('@' + username);
+      userFeed = streams.users[username];
     }
     for (var i = 0; i < userFeed.length; i++) {
       var tweet = userFeed[i];
       var $tweet = $('<div class="tweet"></div>'); //Tweet format is created
       var $tweetData = $('<div id="tweetData"></div>');
-      var $icons = $('<div id="icon-container"></div>');
 
-      $('<img class="user-data profile-photo" src="' + tweet.profilePhotoURL + '">').appendTo($tweetData); //Tweet data is parsed
-      $('<span class="user-data friend interactive">@' + tweet.user + '</span>').appendTo($tweetData);
+
+      $('<img class="profile-photo" src="' + tweet.profilePhotoURL + '">').appendTo($tweetData); //Tweet data is parsed
+      $('<span class="friend interactive">@' + tweet.user + '</span>').appendTo($tweetData);
       $('<span class="timestamp">' + jQuery.timeago(tweet.created_at) + '</span>').appendTo($tweetData);
 
-      $('<i class="fa-solid fa-comment icons comment"></i>').appendTo($icons); //interaction icons are added
-      $('<i class="fa-solid fa-retweet icons retweet"></i>').appendTo($icons);
-      $('<i class="fa-solid fa-heart icons like"></i>').appendTo($icons);
-      $('<i class="fa-solid fa-share-from-square icons share"></i>').appendTo($icons);
 
-      $tweetData.appendTo($tweet); //Parsed data input into tweet format
+
+      $tweetData.appendTo($tweet);
       $('<p class="message">' + tweet.message + '</p>').appendTo($tweet);
-      $icons.appendTo($tweet);
+      tweetIcons.clone().appendTo($tweet);
       $tweet.prependTo($feed); //Tweet added to top of feed
     }
   }
 
-  var homeFeed = function () {
+  var homeFeed = function () { //Renders the home feed
     renderFeed();
-    $('span.user-data').click(function () {
+    $('span.friend').click(function () {
       var user = $(this).parent().find('.friend').text().substring(1);
-      user = streams.users[user];
       renderFeed(user);
     });
   };
@@ -89,12 +99,14 @@ $(document).ready(function(){
   homeFeed();
 
 
-  $("#update-feed").click(function () {
-    homeFeed();
+  $("#update-feed").click(homeFeed); //Updates home feed
+
+  $("#current-feed").click(function () { //updates a specific user's feed
+    var user = $(this).text();
+    if(user === 'Home') {
+      homeFeed();
+    } else { renderFeed(user.substring(1)); }
   });
-
-
-
 
   window.isItBeautifulYet = true;
 });
