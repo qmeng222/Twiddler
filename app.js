16c16d049c7983070dbe5c45789c061720cb9f56
Seth <ssaunar@live.com>
2022-02-11 17:41:12 -0700

    Added ~ functionality for renderFeed, updateFeed, and titleClick

--- app.js
+++ app.js
@@ -1,14 +1,152 @@
 $(document).ready(function(){
+
+  // EXISTING ELEMENTS
   var $app = $('#app');
   $app.html('');
+  jQuery("time.timeago").timeago();
 
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div class="tweet"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($app);
-    index -= 1;
+  // NEW ELEMENTS
+  var $title = $('<h1 class="title">Twiddler</h1>');
+  var $updateFeedButton = $('<button id="update-feed">Update Feed</button>');
+  var $header = $('<header id="header"></header>');
+  var $icons  = $('<div class="icons"></div>');
+  var $feed = $('<div id="feed"></div>');
+
+
+  // EVENT HANDLERS
+
+  var titleClick = function(event) {
+    console.log(event);
+
+    alert('The title of this page is ' + event.target.innerText)
   }
 
+  var handleBackButton = function(event) {
+    console.log(event);
+
+    if ($('#update-feed').text() === 'Back') {
+      $('#update-feed').text('Update Feed');
+    }
+  }
+
+  var handleUsernameClick = function(event) {
+    console.log(event);
+
+    if ($('#update-feed').text() === 'Update Feed') {
+      $('#update-feed').text('Back');
+    }
+  }
+
+  var renderFeed = function(user) {
+    $feed.empty();
+    if (user === undefined) {
+      var index = streams.home.length - 1;
+      while(index >= 0) {
+        // TWEET UI COMPONENTS
+        var tweet = streams.home[index];
+        var $tweet = $('<div class="tweet"></div>');
+        var $tweetIcons = $('<div class="tweetIcons"></div>');
+        var $tweetFooter = $('<div class="tweetFooter"></div>');
+
+        var $message = $('<p class="message"></p>');
+        var $username = $('<span class="username"></span>');
+        var $timestamp = $('<div class ="timestamp"></div>');
+        var $profilePhoto = $('<img class="profile-photo">');
+
+        // ICONS
+        var $comment = $('<i class="icon comment far fa-comments"></i>');
+        var $retweet = $('<i class="icon retweet fas fa-retweet"></i>');
+        var $like = $('<i class="icon like far fa-heart"></i>');
+        var $share = $('<i class="icon share fas fa-share"></i>');
+
+        $profilePhoto.attr('src', tweet.profilePhotoURL)
+        $username.text('@' + tweet.user);
+        $message.text(tweet.message);
+        $timestamp.text($.timeago(tweet.created_at));
+
+        // APPEND TO DOM
+        $profilePhoto.appendTo($tweet);
+        $username.appendTo($tweet);
+        $message.appendTo($tweet);
+        $tweetFooter.appendTo($tweet);
+        $timestamp.appendTo($tweetFooter);
+        $comment.appendTo($tweetIcons);
+        $retweet.appendTo($tweetIcons);
+        $like.appendTo($tweetIcons);
+        $share.appendTo($tweetIcons);
+        $tweetIcons.appendTo($tweetFooter);
+        $tweet.appendTo($feed);
+        index -= 1;
+      }
+    } else {
+
+      var index = streams.users[user].length - 1;
+
+      while(index >= 0) {
+        // TWEET UI COMPONENTS
+        var tweet = streams.users[user][index];
+        var $tweet = $('<div class="tweet"></div>');
+        var $tweetIcons = $('<div class="tweetIcons"></div>');
+        var $tweetFooter = $('<div class="tweetFooter"></div>');
+
+        var $message = $('<p class="message"></p>');
+        var $username = $('<span class="username"></span>');
+        var $profilePhoto = $('<img class="profile-photo">');
+        var $timestamp = $('<span class ="timestamp"></span>');
+
+        // ICONS
+        var $comment = $('<i class="icon comment far fa-comments"></i>');
+        var $retweet = $('<i class="icon retweet fas fa-retweet"></i>');
+        var $like = $('<i class="icon like far fa-heart"></i>');
+        var $share = $('<i class="icon share fas fa-share"></i>');
+
+        $profilePhoto.attr('src', tweet.profilePhotoURL)
+        $username.text('@' + tweet.user);
+        $message.text(tweet.message);
+        $timestamp.text($.timeago(tweet.created_at));
+
+        // APPEND TO DOM
+        $profilePhoto.appendTo($tweet);
+        $username.appendTo($tweet);
+        $message.appendTo($tweet);
+        $timestamp.appendTo($tweet);
+        $comment.appendTo($tweet);
+        $retweet.appendTo($tweet);
+        $like.appendTo($tweet);
+        $share.appendTo($tweet);
+        $tweetFooter.appendTo($tweet);
+        $timestamp.appendTo($tweetFooter);
+        $comment.appendTo($tweetIcons);
+        $retweet.appendTo($tweetIcons);
+        $like.appendTo($tweetIcons);
+        $share.appendTo($tweetIcons);
+        $tweetIcons.appendTo($tweetFooter);
+        $tweet.appendTo($feed);
+
+        index -= 1;
+        $updateBtn.on("click", handleBackBtn);
+      }
+    };
+  };
+
+
+  // EVENT LISTENERS
+  $title.on("click", titleClick);
+
+  $updateFeedButton.on("click", function(event) {
+    renderFeed();
+  });
+
+  $feed.on("click", ".username", handleUsernameClick);
+
+  $updateFeedButton.on("click", handleBackButton);
+
+  // APPEND TO DOM
+  $title.appendTo($app);
+  $updateFeedButton.appendTo($header);
+  $icons.appendTo($header);
+  $header.appendTo($app)
+  $feed.appendTo($app);
+
+  renderFeed();
 });
